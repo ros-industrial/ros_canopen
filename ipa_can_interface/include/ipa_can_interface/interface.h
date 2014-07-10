dd42400075bb1ab5fa9f693568d6efb7e8fb5a01
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2014-07-10 15:31:17 +0200

    restructured ipa_can_driver to specific driver versions

---
+++ ipa_can_interface/include/ipa_can_interface/interface.h
@@ -0,0 +1,158 @@
+#ifndef H_IPA_CAN_INTERFACE
+#define H_IPA_CAN_INTERFACE
+
+#include <boost/array.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+#include "FastDelegate.h"
+
+namespace ipa_can{
+
+/** Header for CAN id an meta data*/
+struct Header{
+    unsigned int id:29; ///< CAN ID (11 or 29 bits valid, depending on is_extended member
+    unsigned int is_error:1; ///< marks an error frame (only used internally)
+    unsigned int is_rtr:1; ///< frame is a remote transfer request
+    unsigned int is_extended:1; ///< frame uses 29 bit CAN identifier 
+    /** check if frame header is valid*/
+    bool isValid(){
+        return id < (is_extended?(1<<29):(1<<11));
+    }
+    /** constructor with default parameters
+        * @param[in] i: CAN id, defaults to 0
+        * @param[in] extended: uses 29 bit identifier, defaults to false
+        * @param[in] rtr: is rtr frame, defaults to false
+        */
+    
+    Header(unsigned int i=0, bool extended = false, bool rtr = false) 
+    : id(i),is_error(0),is_rtr(rtr?1:0), is_extended(extended?1:0) {}
+    operator const unsigned int() const { return *(unsigned int*) this; }
+};
+    
+    
+/** reprentation of a CAN frame */   
+struct Frame: public Header{
+    boost::array<unsigned char, 8> data; ///< array for 8 data bytes with bounds checking
+    unsigned char dlc; ///< len of data
+    
+    /** check if frame header and length are valid*/
+    bool isValid(){
+        return (dlc <= 8)  &&  Header::isValid();
+    }
+    /** 
+     * constructor with default parameters
+     * @param[in] i: CAN id, defaults to 0
+     * @param[in] l: number of data bytes, defaults to 0
+     * @param[in] extended: uses 29 bit identifier, defaults to false
+     * @param[in] rtr: is rtr frame, defaults to false
+     */
+    Frame(const Header &h = Header(), unsigned char l = 0)
+    :Header(h), dlc(l) {}
+};
+
+/** extended error information */
+class State{
+public:
+    enum DriverState{
+        closed, open, ready
+    } driver_state;
+    boost::system::error_code error_code; ///< device access error
+    unsigned int internal_error; ///< driver specific error 
+    
+    State() : driver_state(closed), internal_error(0) {}
+    virtual bool isReady() const { driver_state == ready; }
+    virtual ~State() {}
+};
+
+/** template for Listener interface */
+template <typename T,typename U> class Listener{
+    const T callable_;
+public:
+    typedef U Type;
+    typedef T Callable;
+    typedef boost::shared_ptr<const Listener> Ptr;
+    
+    Listener(const T &callable):callable_(callable){ }
+    void operator()(const U & u) const { callable_(u); }
+    virtual ~Listener() {} 
+};
+
+class Interface{
+public:
+    typedef fastdelegate::FastDelegate1<const Frame&> FrameDelegate;
+    typedef fastdelegate::FastDelegate1<const State&> StateDelegate;
+    typedef Listener<const FrameDelegate, const Frame&> FrameListener;
+    typedef Listener<const StateDelegate, const State&> StateListener;
+
+    /**
+     * initialize interface
+     * 
+     * @param[in] device: driver-specific device name/path
+     * @param[in] bitrate: desired bitrate in bit/s
+     * @return true if device was initialized succesfully, false otherwise
+     */
+    virtual bool init(const std::string &device, unsigned int bitrate) = 0;
+    
+    /**
+     * Recover interface after errors and emergency stops
+     * 
+     * @return true if device was recovered succesfully, false otherwise
+     */
+    virtual bool recover() = 0;
+
+    /**
+     * enqueue frame for sending
+     * 
+     * @param[in] msg: message to be enqueued
+     * @return true if frame was enqueued succesfully, otherwise false
+     */
+    virtual bool send(const Frame & msg) = 0;
+    
+    /**
+     * @return current state of driver
+     */
+    virtual State getState() = 0;
+
+    /**
+     * shutdown interface
+     *
+     * @return true if shutdown was succesful, false otherwise
+     */
+    virtual void shutdown() = 0;
+    
+    /**
+     * acquire a listener for the specified delegate, that will get called for all messages
+     * 
+     * @param[in] delegate: delegate to be bound by the listener
+     * @return managed pointer to listener
+     */
+    virtual FrameListener::Ptr createMsgListener(const FrameDelegate &delegate) = 0;
+    
+    /**
+     * acquire a listener for the specified delegate, that will get called for messages with demanded ID
+     * 
+     * @param[in] header: CAN header to restrict listener on
+     * @param[in] delegate: delegate to be bound listener
+     * @return managed pointer to listener
+     */
+    virtual FrameListener::Ptr createMsgListener(const Frame::Header&, const FrameDelegate &delegate) = 0;
+    
+    /**
+     * acquire a listener for the specified delegate, that will get called for all state changes
+     * 
+     * @param[in] delegate: delegate to be bound by the listener
+     * @return managed pointer to listener
+     */
+    virtual StateListener::Ptr createStateListener(const StateDelegate &delegate) = 0;
+    
+    virtual bool translateError(unsigned int internal_error, std::string & str) = 0;
+    
+    virtual void run() {}
+    
+    virtual ~Interface() {}
+};
+
+}; // namespace ipa_can
+#endif
---
+++ ipa_socketcan_driver/include/ipa_socketcan_driver/socketcan.h
@@ -0,0 +1,148 @@
+#ifndef H_IPA_SOCKETCAN_DRIVER
+#define H_IPA_SOCKETCAN_DRIVER
+
+#include <ipa_can_interface/asio_base.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+ 
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <linux/can/error.h>
+
+namespace ipa_can {
+
+template<typename FrameDelegate, typename StateDelegate> class SocketCANDriver : public AsioDriver<FrameDelegate,StateDelegate,boost::asio::posix::stream_descriptor> {
+    typedef AsioDriver<FrameDelegate,StateDelegate,boost::asio::posix::stream_descriptor> BaseClass;
+public:    
+    SocketCANDriver(FrameDelegate frame_delegate, StateDelegate state_delegate)
+    : BaseClass(frame_delegate, state_delegate)
+    {}
+    bool init(const std::string &device, unsigned int bitrate){
+        State s = BaseClass::getState();
+        if(s.driver_state == State::closed){
+            device_ = device;
+            if(bitrate != 0) return false; // not supported, TODO: use libsocketcan
+
+            int sc = socket( PF_CAN, SOCK_RAW, CAN_RAW );
+            std::cout << "sc " << sc << std::endl;
+            if(sc < 0){
+                BaseClass::setErrorCode(boost::system::error_code(sc,boost::system::system_category()));
+                return false;
+            }
+            
+            struct ifreq ifr;
+            strcpy(ifr.ifr_name, device_.c_str());
+            int ret = ioctl(sc, SIOCGIFINDEX, &ifr);
+
+            std::cout << "ret1 " << ret << std::endl;
+            if(ret != 0){
+                BaseClass::setErrorCode(boost::system::error_code(ret,boost::system::system_category()));
+                close(sc);
+                return false;
+            }
+
+            can_err_mask_t err_mask = ( CAN_ERR_TX_TIMEOUT | CAN_ERR_BUSOFF ); //TODO select errors to track
+
+            ret = setsockopt(sc, SOL_CAN_RAW, CAN_RAW_ERR_FILTER,
+               &err_mask, sizeof(err_mask));
+            
+            if(ret != 0){
+                BaseClass::setErrorCode(boost::system::error_code(ret,boost::system::system_category()));
+                close(sc);
+                return false;
+            }
+            
+            struct sockaddr_can addr;
+            addr.can_family = AF_CAN;
+            addr.can_ifindex = ifr.ifr_ifindex;
+            ret = bind( sc, (struct sockaddr*)&addr, sizeof(addr) );            
+
+            std::cout << "ret2 " << ret << std::endl;
+            if(ret != 0){
+                BaseClass::setErrorCode(boost::system::error_code(ret,boost::system::system_category()));
+                close(sc);
+                return false;
+            }
+            
+            boost::system::error_code ec;
+            BaseClass::socket_.assign(sc,ec);
+            
+            BaseClass::setErrorCode(ec);
+            
+            if(ec){
+                std::cout << "!ASSIGN"<< std::endl;
+                close(sc);
+                return false;
+            }
+            BaseClass::setDriverState(State::open);
+            return true;
+        }else  std::cout << "NO CLOSED" << std::endl;
+        return false;
+    }
+    bool recover(){
+        State s = BaseClass::getState();
+        if(s.driver_state == State::open){
+            BaseClass::shutdown();
+            return init(device_, 0);
+        }
+    }
+    bool translateError(unsigned int internal_error, std::string & str){
+        return false; // TODO
+    }
+protected:
+    std::string device_;
+    can_frame frame_;
+    
+    void triggerReadSome(){
+        BaseClass::socket_.async_read_some(boost::asio::buffer(&frame_, sizeof(frame_)), boost::bind( &SocketCANDriver::readFrame,this, boost::asio::placeholders::error));
+    }
+    
+    bool enqueue(const Frame & msg){
+        boost::mutex::scoped_lock lock(send_mutex_); //TODO: timed try lock
+
+        can_frame frame;
+        frame.can_id = msg.id | (msg.is_extended?CAN_EFF_FLAG:0) | (msg.is_rtr?CAN_RTR_FLAG:0);;
+        frame.can_dlc = msg.dlc;
+        
+        
+        for(int i=0; i < frame.can_dlc;++i)
+            frame.data[i] = msg.data[i];
+        
+        boost::system::error_code ec;
+        boost::asio::write(BaseClass::socket_, boost::asio::buffer(&frame, sizeof(frame)),boost::asio::transfer_all(), ec);
+        if(ec){
+            BaseClass::setErrorCode(ec);
+            BaseClass::setDriverState(State::open);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    void readFrame(const boost::system::error_code& error){
+        if(!error){
+            if(frame_.can_id & CAN_ERR_FLAG){ // error message
+                // TODO
+                std::cout << "error" << std::endl;
+            }
+
+            BaseClass::input_.is_extended = frame_.can_id & CAN_EFF_FLAG;
+            BaseClass::input_.id = frame_.can_id & (BaseClass::input_.is_extended ? CAN_EFF_MASK : CAN_SFF_MASK);
+            BaseClass::input_.is_error = frame_.can_id & CAN_ERR_FLAG;
+            BaseClass::input_.is_rtr = frame_.can_id & CAN_RTR_FLAG;
+            BaseClass::input_.dlc = frame_.can_dlc;
+            for(int i=0;i<frame_.can_dlc && i < 8; ++i){
+                BaseClass::input_.data[i] = frame_.data[i];
+            }
+        }
+        BaseClass::frameReceived(error);
+    }
+private:
+    boost::mutex send_mutex_;
+};
+    
+}; // namespace ipa_can
+#endif
