54e5c1477800330cebf83ace473be47f24428ebc
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2014-07-31 18:14:27 +0200

    PDO parsing fallback

--- ipa_canopen_master/src/objdict.cpp
+++ ipa_canopen_master/src/objdict.cpp
@@ -216,7 +216,12 @@ void parse_object(boost::shared_ptr<ObjectDict> dict, boost::property_tree::ptre
                 }
             }else{
                 //subs = object->get<uint8_t>("SubNumber");
-                subs = pt.get<uint8_t>(name.substr(2) + "sub0.DefaultValue") + 1;
+                try{
+                    subs = pt.get<uint8_t>(name.substr(2) + "sub0.DefaultValue") + 1;
+                }
+                catch(...){
+                    subs = object->get<uint8_t>("SubNumber");
+                }
                 for(uint8_t i=0; i< subs; ++i){
                    
                    parse_object(dict, pt, name + "sub" + boost::lexical_cast<std::string>((int)i), &i);
