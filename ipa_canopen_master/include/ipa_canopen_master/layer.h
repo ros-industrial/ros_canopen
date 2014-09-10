e37d9f0318fe2a87cceb05849037b51d9fc9a982
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2014-09-10 20:47:57 +0200

    bugfixed for Layers

--- ipa_canopen_master/include/ipa_canopen_master/layer.h
+++ ipa_canopen_master/include/ipa_canopen_master/layer.h
@@ -13,13 +13,13 @@ public:
     virtual bool init() = 0;
     virtual bool recover() = 0;
     virtual bool shutdown() = 0;
-    virtual ~Layer();
+    virtual ~Layer() {}
 };
 
-class LayerVector{
-    typedef std::vector<boost::shared_ptr<Layer> > vector_type ;
+template<typename T> class LayerVector{
+    typedef std::vector<boost::shared_ptr<T> > vector_type ;
     vector_type layers;
-    template<typename Iterator> bool call(bool(Layer::*func)(void), const Iterator &begin, const Iterator &end){
+    template<typename Iterator> bool call(bool(T::*func)(void), const Iterator &begin, const Iterator &end){
         bool res = true;
         for(Iterator it = begin; it != end; ++it){
             res = ((**it).*func)() && res;
@@ -27,47 +27,47 @@ class LayerVector{
         return res;
     }
 public:
-    bool call_fwd(bool(Layer::*func)(void)){
+    bool call_fwd(bool(T::*func)(void)){
         return call(func, layers.begin(), layers.end());
     }
-    bool call_rev(bool(Layer::*func)(void)){
+    bool call_rev(bool(T::*func)(void)){
         return call(func, layers.rbegin(), layers.rend());
     }
-    void add(const boost::shared_ptr<Layer> &l) { layers.push_back(l); }
+    void add(const boost::shared_ptr<T> &l) { layers.push_back(l); }
 };
     
-class LayerStack : public Layer, public LayerVector{
+class LayerStack : public Layer, public LayerVector<Layer>{
 public:
-    virtual bool read() { return call_fwd(&Layer::read); }
-    virtual bool write() { return call_rev(&Layer::write); }
-    virtual bool report() { return call_fwd(&Layer::report); }
-    virtual bool init() { return call_fwd(&Layer::init); }
-    virtual bool recover() { return call_fwd(&Layer::recover); }
-    virtual bool shutdown() { return call_rev(&Layer::shutdown); }
+    virtual bool read() { return this->call_fwd(&Layer::read); }
+    virtual bool write() { return this->call_rev(&Layer::write); }
+    virtual bool report() { return this->call_fwd(&Layer::report); }
+    virtual bool init() { return this->call_fwd(&Layer::init); }
+    virtual bool recover() { return this->call_fwd(&Layer::recover); }
+    virtual bool shutdown() { return this->call_rev(&Layer::shutdown); }
 };
-class LayerGroup : public Layer, public LayerVector{
+template<typename T> class LayerGroup : public Layer, public LayerVector<T>{
 public:
-    virtual bool read() { return call_fwd(&Layer::read); }
-    virtual bool write() { return call_fwd(&Layer::write); }
-    virtual bool report() { return call_fwd(&Layer::report); }
-    virtual bool init() { return call_fwd(&Layer::init); }
-    virtual bool recover() { return call_fwd(&Layer::recover); }
-    virtual bool shutdown() { return call_fwd(&Layer::shutdown); }
+    virtual bool read() { return this->call_fwd(&Layer::read); }
+    virtual bool write() { return this->call_fwd(&Layer::write); }
+    virtual bool report() { return this->call_fwd(&Layer::report); }
+    virtual bool init() { return this->call_fwd(&Layer::init); }
+    virtual bool recover() { return this->call_fwd(&Layer::recover); }
+    virtual bool shutdown() { return this->call_fwd(&Layer::shutdown); }
 };
 
-class CANLayer: public Layer{
-    boost::shared_ptr<ipa_can::DriverInterface> driver_;
+template<typename Driver> class CANLayer: public Layer{
+    boost::shared_ptr<Driver> driver_;
     const std::string device_;
     const unsigned int bitrate_;
 public:
-    CANLayer(const boost::shared_ptr<ipa_can::DriverInterface> &driver, const std::string &device, const unsigned int bitrate)
+    CANLayer(const boost::shared_ptr<Driver> &driver, const std::string &device, const unsigned int bitrate)
     : driver_(driver), device_(device), bitrate_(bitrate) { assert(driver_); }
     virtual bool read() { return driver_->getState().isReady(); }
     virtual bool write() { return driver_->getState().isReady(); }
     virtual bool report() { return false; }
-    virtual bool init() { driver_->init(device_, bitrate_); }
+    virtual bool init() { return driver_->init(device_, bitrate_); }
     virtual bool recover() { return driver_->recover(); }
-    virtual bool shutdown() { driver_->shutdown(); }
+    virtual bool shutdown() { driver_->shutdown(); return true;}
 };
 } // namespace ipa_canopen
 
