e6817bb953f998bf3b22d7527b325b36acadb1b9
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2014-09-10 21:05:15 +0200

    ROS chain as LayerStack

--- ipa_canopen_chain_ros/include/ipa_canopen_chain_ros/chain_ros.h
+++ ipa_canopen_chain_ros/include/ipa_canopen_chain_ros/chain_ros.h
@@ -2,6 +2,7 @@
 #define H_IPA_CANOPEN_CHAIN_ROS
 
 #include <ipa_canopen_master/canopen.h>
+#include <ipa_canopen_master/master.h>
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <cob_srvs/Trigger.h>
@@ -45,22 +46,25 @@ public:
         boost::shared_ptr<NodeType> node = node_.lock();
         if(node){
             stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Active");
-            for(size_t i=0; i < entries_.size(); ++i) entries_[i](stat);
+            if(node->getState() == ipa_canopen::Node::Operational)
+                for(size_t i=0; i < entries_.size(); ++i) entries_[i](stat);
             //ROS_INFO_STREAM(node->getStorage()->template entry<std::string>(0x1008).get_once());
             //stat.add("desc",std::string());
+
         }
     }
     virtual ~Logger() {}
 };
 
-template<typename NodeType, typename InterfaceType, typename MasterType> class RosChain {
+template<typename InterfaceType, typename MasterType> class RosChain : public ipa_canopen::LayerStack {
 protected:
-    
     std::string chain_name_;
     
     boost::shared_ptr<InterfaceType> interface_;
     boost::shared_ptr<MasterType> master_;
-    boost::shared_ptr<NodeChain<NodeType> > nodes_;
+    boost::shared_ptr<ipa_canopen::LayerGroup<ipa_canopen::Node> > nodes_;
+    boost::shared_ptr<ipa_canopen::SyncLayer> sync_;
+
     ipa_can::StateInterface::StateListener::Ptr state_listener_;
     
     boost::scoped_ptr<boost::thread> thread_;
@@ -73,7 +77,6 @@ protected:
 
     boost::mutex mutex_;
     ros::ServiceServer srv_init_;
-    ros::ServiceServer srv_halt_;
     ros::ServiceServer srv_recover_;
     ros::ServiceServer srv_shutdown_;
     
@@ -84,30 +87,43 @@ protected:
         ROS_INFO_STREAM("Current state: " << s.driver_state << " device error: " << s.error_code << " internal_error: " << s.internal_error << " (" << msg << ")");
     }
     
-    void _destroy(){
-        state_listener_.reset();
+    void run(){
+        while(ros::ok()){
+            read();
+            write();
+            boost::this_thread::interruption_point();
+        }
+    }
+    
+    virtual bool handle_init(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
+        boost::mutex::scoped_lock lock(mutex_);
+        res.success.data = init();
+        thread_.reset(new boost::thread(&RosChain::run, this));
+        return true;
+    }
+    virtual bool handle_recover(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
+        boost::mutex::scoped_lock lock(mutex_);
+        res.success.data = recover();
+        return true;
+    }
+    virtual bool handle_shutdown(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
+        boost::mutex::scoped_lock lock(mutex_);
         if(thread_){
-            interface_->shutdown();
+            thread_->interrupt();
             thread_->join();
         }
         thread_.reset();
-        nodes_.reset();
-        master_.reset();
-        interface_.reset();
-        
+        res.success.data = shutdown();
+        return true;
     }
-    virtual bool handle_init(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
-        boost::mutex::scoped_lock lock(mutex_);
-        res.success.data = false;
-        
+    bool setup_bus(){
         ros::NodeHandle bus_nh(nh_priv_,"bus");
         std::string can_device;
         int can_bitrate = 0;
         
         if(!bus_nh.getParam("device",can_device)){
-            _destroy();
             ROS_ERROR("Device not set");
-            return true;
+            return false;
         }
         
         bool can_loopback;
@@ -115,9 +131,8 @@ protected:
         bus_nh.param("bitrate",can_bitrate, 0);
         
         if(can_bitrate < 0){
-            _destroy();
             ROS_ERROR_STREAM("CAN bitrate  "<< can_bitrate << " is invalid");
-            return true;
+            return false;
         }
         
         bus_nh.param("loopback",can_loopback, true);
@@ -125,24 +140,14 @@ protected:
         interface_ = boost::make_shared<InterfaceType>(can_loopback);
         state_listener_ = interface_->createStateListener(ipa_can::StateInterface::StateDelegate(this, &RosChain::logState));
         
-        if(!interface_->init(can_device, can_bitrate)){
-            _destroy();
-            ROS_ERROR_STREAM("Initializing can device "<< can_device << " failed");
-            return true;
-        }
-        
-        thread_.reset(new boost::thread(&InterfaceType::run, interface_));
-        
-        if(!StateWaiter::wait_for(ipa_can::State::ready, interface_, boost::posix_time::seconds(1))){
-            _destroy();
-            ROS_ERROR_STREAM("Initializing can device "<< can_device << " failed");
-            return true;
-        }
-        
         master_ = boost::make_shared<MasterType>(interface_);
         
+        add(boost::make_shared<CANLayer<InterfaceType> >(interface_, can_device, can_bitrate));
+        
+        return true;
+    }
+    bool setup_sync(){
         ros::NodeHandle sync_nh(nh_priv_,"sync");
-        boost::shared_ptr<SyncProvider> sync;
         
         int sync_ms = 0;
         int sync_overflow = 0;
@@ -152,99 +157,78 @@ protected:
         }
         
         if(sync_ms < 0){
-            _destroy();
             ROS_ERROR_STREAM("Sync interval  "<< sync_ms << " is invalid");
-            return true;
+            return false;
         }
         
         if(!sync_nh.getParam("overflow", sync_overflow)){
             ROS_WARN("Sync overflow was not specified, so overflow is disabled per default");
         }
         if(sync_overflow < 0 || sync_overflow > 255){
-            _destroy();
             ROS_ERROR_STREAM("Sync overflow  "<< sync_overflow << " is invalid");
-            return true;
+            return false;
         }
 
         if(sync_ms){
             // TODO: parse header
-            sync = master_->getSync(ipa_can::Header(0x80), boost::posix_time::milliseconds(sync_ms), sync_overflow, can_loopback);
+            sync_ = master_->getSync(ipa_can::Header(0x80), boost::posix_time::milliseconds(sync_ms), sync_overflow);
             
-            if(!sync && sync_ms){
-                _destroy();
-                ROS_ERROR_STREAM("Initializing sync master "<< can_device << " failed");
-                return true;
+            if(!sync_ && sync_ms){
+                ROS_ERROR_STREAM("Initializing sync master failed");
+                return false;
             }
         }
-        
-        nodes_.reset(new ipa_canopen::NodeChain<NodeType>);
+        add(sync_);
+        return true;
+    }
+    bool setup_nodes(){
+        nodes_.reset(new ipa_canopen::LayerGroup<ipa_canopen::Node>);
 
         XmlRpc::XmlRpcValue modules;
         nh_priv_.getParam("modules", modules);
         ros::NodeHandle def_nh(nh_priv_,"defaults");
 
-        try{
-            for (int32_t i = 0; i < modules.size(); ++i){
-                XmlRpc::XmlRpcValue &module = modules[i];
-                std::string name = module["name"];
-                int node_id;
-                try{
-                node_id = module["id"];
-                }
-                catch(...){
-                    _destroy();
-                    ROS_ERROR_STREAM("Module at list index " << i << " has no id");
-                    return true;
-                }
-                std::string eds;
-                std::string pkg;
-                
-                if(!read_xmlrpc_or_praram(eds, "eds_file", module, def_nh) || eds.empty()){
-                    _destroy();
-                    ROS_ERROR_STREAM("EDS path '" << eds << "' invalid");
-                    return true;
-                }
-                if(read_xmlrpc_or_praram(pkg, "eds_pkg", module, def_nh)){
-                std::string p = ros::package::getPath(pkg);
-                if(p.empty()){
-                        _destroy();
-                        ROS_ERROR_STREAM("Package '" << pkg << "' not found");
-                        return true;
-                }
-                eds = (boost::filesystem::path(p)/eds).make_preferred().native();;
-                }
-                boost::shared_ptr<ObjectDict>  dict = ObjectDict::fromFile(eds);
-                if(!dict){
-                    _destroy();
-                    ROS_ERROR_STREAM("EDS '" << eds << "' could not be parsed");
-                    return true;
-                }
-                boost::shared_ptr<NodeType> node = boost::make_shared<NodeType>(interface_, dict, node_id, sync);
-                boost::shared_ptr<Logger<NodeType> > logger = boost::make_shared<Logger<NodeType> >(node);
-                diag_updater_.add(name, boost::bind(&Logger<NodeType>::log, logger, _1));
-                
-                nodes_->add(node);
+        for (int32_t i = 0; i < modules.size(); ++i){
+            XmlRpc::XmlRpcValue &module = modules[i];
+            std::string name = module["name"];
+            int node_id;
+            try{
+            node_id = module["id"];
             }
-            nodes_->start();
+            catch(...){
+                ROS_ERROR_STREAM("Module at list index " << i << " has no id");
+                return false;
+            }
+            std::string eds;
+            std::string pkg;
+            
+            if(!read_xmlrpc_or_praram(eds, "eds_file", module, def_nh) || eds.empty()){
+                ROS_ERROR_STREAM("EDS path '" << eds << "' invalid");
+                return false;
+            }
+            if(read_xmlrpc_or_praram(pkg, "eds_pkg", module, def_nh)){
+            std::string p = ros::package::getPath(pkg);
+            if(p.empty()){
+                    ROS_ERROR_STREAM("Package '" << pkg << "' not found");
+                    return false;
+            }
+            eds = (boost::filesystem::path(p)/eds).make_preferred().native();;
+            }
+            boost::shared_ptr<ObjectDict>  dict = ObjectDict::fromFile(eds);
+            if(!dict){
+                ROS_ERROR_STREAM("EDS '" << eds << "' could not be parsed");
+                return false;
+            }
+            boost::shared_ptr<ipa_canopen::Node> node = boost::make_shared<ipa_canopen::Node>(interface_, dict, node_id, sync_);
+
+            boost::shared_ptr<Logger<ipa_canopen::Node> > logger = boost::make_shared<Logger<ipa_canopen::Node> >(node);
+            diag_updater_.add(name, boost::bind(&Logger<Node>::log, logger, _1));
+            
+            nodes_->add(node);
         }
-        catch(TimeoutException){
-           ROS_ERROR("Timeout");
-           return true;
-        }
-        res.success.data = true;
+        add(nodes_);
         return true;
-    }
-    virtual bool handle_halt(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
-        boost::mutex::scoped_lock lock(mutex_);
-        return true;
-    }
-    virtual bool handle_recover(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
-        boost::mutex::scoped_lock lock(mutex_);
-        return true;
-    }
-    virtual bool handle_shutdown(cob_srvs::Trigger::Request  &req, cob_srvs::Trigger::Response &res){
-        boost::mutex::scoped_lock lock(mutex_);
-        return true;
+        
     }
 public:
     RosChain(const ros::NodeHandle &nh, const ros::NodeHandle &nh_priv): nh_(nh), nh_priv_(nh_priv), diag_updater_(nh_,nh_priv_){}
@@ -264,15 +248,13 @@ public:
         ros::NodeHandle nh_chain(nh_, chain_name_);
         
         srv_init_ = nh_chain.advertiseService("init",&RosChain::handle_init, this);
-        srv_halt_ = nh_chain.advertiseService("halt",&RosChain::handle_halt, this);
         srv_recover_ = nh_chain.advertiseService("recover",&RosChain::handle_recover, this);
         srv_shutdown_ = nh_chain.advertiseService("shutdown",&RosChain::handle_shutdown, this);
         
-        return true;
+        return setup_bus() && setup_sync() && setup_nodes();
     }
     virtual ~RosChain(){
-        diag_timer_.stop();
-        _destroy();
+        shutdown();
     }
 };
 
