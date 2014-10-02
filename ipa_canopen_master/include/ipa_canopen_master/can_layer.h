3e88fa3c2f929c9435b393be595aec514b1e7832
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2014-10-02 19:25:25 +0200

    report CAN errors

--- ipa_canopen_master/include/ipa_canopen_master/can_layer.h
+++ ipa_canopen_master/include/ipa_canopen_master/can_layer.h
@@ -6,9 +6,18 @@
 namespace ipa_canopen{
     
 template<typename Driver> class CANLayer: public Layer{
+    boost::mutex mutex_;
     boost::shared_ptr<Driver> driver_;
     const std::string device_;
     const unsigned int bitrate_;
+    ipa_can::Frame last_error_;
+    ipa_can::CommInterface::FrameListener::Ptr error_listener_;
+    void handleFrame(const ipa_can::Frame & msg){
+        boost::mutex::scoped_lock lock(mutex_);
+        last_error_ = msg;
+        LOG("ID: " << msg.id);
+    }
+
 public:
     CANLayer(const boost::shared_ptr<Driver> &driver, const std::string &device, const unsigned int bitrate)
     : Layer(device + " Layer"), driver_(driver), device_(device), bitrate_(bitrate) { assert(driver_); }
@@ -20,7 +29,6 @@ public:
     }
 
     virtual void report(LayerStatusExtended &status){
-        // TODO: implement history
         ipa_can::State s = driver_->getState();
         if(!s.isReady()){
             status.error("CAN layer not ready");
@@ -30,10 +38,19 @@ public:
             status.add("socket_error", s.error_code);
         }
         if(s.internal_error != 0){
-            // TODO: interprete error
             status.add("internal_error", int(s.internal_error));
             std::string desc;
             if(driver_->translateError(s.internal_error, desc)) status.add("internal_error_desc", desc);
+            std::stringstream sstr;
+            sstr << std::hex;
+            {
+                boost::mutex::scoped_lock lock(mutex_);
+                for(size_t i=0; i < last_error_.dlc; ++i){
+                    sstr << (unsigned int) last_error_.data[i] << " ";
+                }
+            }
+            status.add("can_error_frame", sstr.str());
+
         }
 
     }
@@ -41,9 +58,12 @@ public:
     virtual void init(LayerStatusExtended &status){
         if(!driver_->init(device_, bitrate_)){
             status.error("CAN init failed");
+        }else{
+            error_listener_ = driver_->createMsgListener(ipa_can::ErrorHeader(), ipa_can::CommInterface::FrameDelegate(this, &CANLayer::handleFrame));
         }
     }
     virtual void shutdown(LayerStatus &status){
+        error_listener_.reset();
         driver_->shutdown();
     }
 
