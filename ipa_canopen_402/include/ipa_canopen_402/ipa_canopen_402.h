ce83762219b8b82f503d724a1eea97632844dd1b
thiagodefreitas <thiagodefreitas@gmail.com>
2014-10-21 13:53:11 +0200

    License and more readable version of homing

--- ipa_canopen_402/include/ipa_canopen_402/ipa_canopen_402.h
+++ ipa_canopen_402/include/ipa_canopen_402/ipa_canopen_402.h
@@ -1,7 +1,64 @@
-#ifndef H_IPA_CANOPEN_402
-#define H_IPA_CANOPEN_402
+/****************************************************************
+ *
+ * Copyright (c) 2014
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: ipa_canopen_402
+ * ROS stack name: ipa_canopen_402
+ * ROS package name: ipa_canopen_402
+ * Description: This class implements the CANopen device profile for
+ * drives and motion control
+ * CiA (r) 402
+ * Standardized in IEC 61800-7-201 and IEC 61800-7-301
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Thiago de Freitas, email:tdf@ipa.fhg.de
+ * Supervised by: Thiago de Freitas, email:tdf@ipa.fhg.de
+ *
+ * Date of creation: July 2014
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+#ifndef IPA_CANOPEN_402_IPA_CANOPEN_402_H
+#define IPA_CANOPEN_402_IPA_CANOPEN_402_H
 
 #include <ipa_canopen_master/canopen.h>
+#include <string>
+#include <vector>
 
 namespace ipa_canopen
 {
@@ -21,7 +78,7 @@ public:
 
     homing_mask.set(SW_Target_reached);
     homing_mask.set(SW_Operation_specific0);
-    homing_mask.set(SW_Operation_specific0);
+    homing_mask.set(SW_Operation_specific1);
 
     homing_needed_ = false;
     motor_ready_ = false;
@@ -29,42 +86,42 @@ public:
 
   enum StatusWord
   {
-    SW_Ready_To_Switch_On,
-    SW_Switched_On,
-    SW_Operation_enabled,
-    SW_Fault,
-    SW_Voltage_enabled,
-    SW_Quick_stop,
-    SW_Switch_on_disabled,
-    SW_Warning,
-    SW_Manufacturer_specific0,
-    SW_Remote,
-    SW_Target_reached,
-    SW_Internal_limit,
-    SW_Operation_specific0,
-    SW_Operation_specific1,
-    SW_Manufacturer_specific1,
-    SW_Manufacturer_specific2
+    SW_Ready_To_Switch_On=0,
+    SW_Switched_On=1,
+    SW_Operation_enabled=2,
+    SW_Fault=3,
+    SW_Voltage_enabled=4,
+    SW_Quick_stop=5,
+    SW_Switch_on_disabled=6,
+    SW_Warning=7,
+    SW_Manufacturer_specific0=8,
+    SW_Remote=9,
+    SW_Target_reached=10,
+    SW_Internal_limit=11,
+    SW_Operation_specific0=12,
+    SW_Operation_specific1=13,
+    SW_Manufacturer_specific1=14,
+    SW_Manufacturer_specific2=15
   };
 
   enum ControlWord
   {
-    CW_Switch_On,
-    CW_Enable_Voltage,
-    CW_Quick_Stop,
-    CW_Enable_Operation,
-    CW_Operation_mode_specific0,
-    CW_Operation_mode_specific1,
-    CW_Operation_mode_specific2,
-    CW_Fault_Reset,
-    CW_Halt,
-    CW_Reserved0,
-    CW_Reserved1,
-    CW_Manufacturer_specific0,
-    CW_Manufacturer_specific1,
-    CW_Manufacturer_specific2,
-    CW_Manufacturer_specific3,
-    CW_Manufacturer_specific4,
+    CW_Switch_On=0,
+    CW_Enable_Voltage=1,
+    CW_Quick_Stop=2,
+    CW_Enable_Operation=3,
+    CW_Operation_mode_specific0=4,
+    CW_Operation_mode_specific1=5,
+    CW_Operation_mode_specific2=6,
+    CW_Fault_Reset=7,
+    CW_Halt=8,
+    CW_Reserved0=9,
+    CW_Reserved1=10,
+    CW_Manufacturer_specific0=11,
+    CW_Manufacturer_specific1=12,
+    CW_Manufacturer_specific2=13,
+    CW_Manufacturer_specific3=14,
+    CW_Manufacturer_specific4=15,
   };
 
   enum OperationMode
@@ -74,7 +131,7 @@ public:
     Velocity = 2,
     Profiled_Velocity = 3,
     Profiled_Torque = 4,
-    Reserved=5,
+    Reserved = 5,
     Homing = 6,
     Interpolated_Position = 7,
     Cyclic_Synchronous_Position = 8,
@@ -98,15 +155,15 @@ public:
 
   enum State
   {
-    Start=0,
-    Not_Ready_To_Switch_On=1,
-    Switch_On_Disabled=2,
-    Ready_To_Switch_On=3,
-    Switched_On=4,
-    Operation_Enable=5,
-    Quick_Stop_Active=6,
-    Fault_Reaction_Active=7,
-    Fault=8,
+    Start = 0,
+    Not_Ready_To_Switch_On = 1,
+    Switch_On_Disabled = 2,
+    Ready_To_Switch_On = 3,
+    Switched_On = 4,
+    Operation_Enable = 5,
+    Quick_Stop_Active = 6,
+    Fault_Reaction_Active = 7,
+    Fault = 8,
   };
 
   const OperationMode getMode();
@@ -128,7 +185,7 @@ public:
   virtual void init(LayerStatus &status);
   virtual void shutdown(LayerStatus &status);
 
-  void getDeviceState();
+  void getDeviceState(LayerStatus &status);
   void switchMode(LayerStatus &status);
 
   void motorShutdown();
@@ -151,11 +208,14 @@ public:
 
   void setTargetPos(const double &target_pos);
   void setTargetVel(const double &target_vel);
-  void setTargetEff(const double &v) {} //TODO
+  void setTargetEff(const double &v) {}  // TODO(thiagodefreitas)
 
   const double getTargetPos();
   const double getTargetVel();
-  const double getTargetEff()  { return 0; } // TODO
+  const double getTargetEff()
+  {
+    return 0;  // TODO(thiagodefreitas)
+  }
 
   bool turnOn();
   bool turnOff();
@@ -217,7 +277,6 @@ private:
   double target_pos_;
 
   std::vector<int> control_word_buffer;
-
 };
 }  //  namespace ipa_canopen
-#endif  // H_IPA_CANOPEN_402
+#endif  // IPA_CANOPEN_402_IPA_CANOPEN_402_H
--- ipa_canopen_402/src/ipa_canopen_402/ipa_canopen_402.cpp
+++ ipa_canopen_402/src/ipa_canopen_402/ipa_canopen_402.cpp
@@ -1,6 +1,61 @@
+/****************************************************************
+ *
+ * Copyright (c) 2014
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: ipa_canopen_402
+ * ROS stack name: ipa_canopen_402
+ * ROS package name: ipa_canopen_402
+ * Description: This class implements the CANopen device profile for
+ * drives and motion control
+ * CiA (r) 402
+ * Standardized in IEC 61800-7-201 and IEC 61800-7-301
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Thiago de Freitas, email:tdf@ipa.fhg.de
+ * Supervised by: Thiago de Freitas, email:tdf@ipa.fhg.de
+ *
+ * Date of creation: July 2014
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
 #include <ipa_canopen_402/ipa_canopen_402.h>
 
-using namespace ipa_canopen;
+using ipa_canopen::Node_402;
 
 void Node_402::pending(LayerStatus &status)
 {
@@ -10,25 +65,25 @@ void Node_402::pending(LayerStatus &status)
   ac_pos_ = actual_pos.get();
   ac_vel_ = 0;
 
-  getDeviceState();
+  getDeviceState(status);
 
   operation_mode_ = (OperationMode) op_mode_display.get();
 
-  if(check_mode)
+  if (check_mode)
   {
     switchMode(status);
   }
 
   if (state_ != target_state_)
   {
-    switch(state_)
+    switch (state_)
     {
     case Fault:
       motorFaultReset();
       break;
     case Not_Ready_To_Switch_On:
     case Switch_On_Disabled:
-      switch(target_state_)
+      switch (target_state_)
       {
       case Ready_To_Switch_On:
       case Switched_On:
@@ -38,7 +93,7 @@ void Node_402::pending(LayerStatus &status)
       }
       break;
     case Ready_To_Switch_On:
-      switch(target_state_)
+      switch (target_state_)
       {
       case Switch_On_Disabled:
         motorDisableVoltage();
@@ -50,7 +105,7 @@ void Node_402::pending(LayerStatus &status)
       }
       break;
     case Switched_On:
-      switch(target_state_)
+      switch (target_state_)
       {
       case Switch_On_Disabled:
         motorQuickStop();
@@ -64,7 +119,7 @@ void Node_402::pending(LayerStatus &status)
       }
       break;
     case Operation_Enable:
-      switch(target_state_)
+      switch (target_state_)
       {
       case Switch_On_Disabled:
         motorDisableVoltage();
@@ -77,7 +132,7 @@ void Node_402::pending(LayerStatus &status)
       }
       break;
     case Quick_Stop_Active:
-      switch(target_state_)
+      switch (target_state_)
       {
       case Switch_On_Disabled:
         motorDisableVoltage();
@@ -90,37 +145,51 @@ void Node_402::pending(LayerStatus &status)
     }
     status.warn();
   }
-  else if(homing_needed_)
+  else if (homing_needed_)
   {
     op_mode.set(Homing);
 
-    if(operation_mode_ == Homing)
+    if (operation_mode_ == Homing)
     {
       control_word_bitset.set(CW_Operation_mode_specific0);
 
-      switch((status_word_bitset & homing_mask).to_ulong())
+      switch ((status_word_bitset & homing_mask).to_ulong())
       {
-      case 0b0000000000000000:
+      //-------------------------------------------------------------
+      // Op_specific1 | Op_specific0 | Target_reached | Description |
+      // ------------ | ------------ | -------------- | ----------- |
+      //       0      |       0      |         0      | In Progress |
+      //       0      |       0      |         1      | Hom.Started |
+      //       0      |       1      |         0      | Hom.Attained, target not reached|
+      //       0      |       1      |         1      | Hom.Succesful |
+      //       1      |       0      |         0      | Hom.Error, vel!=0 |
+      //       1      |       0      |         1      | Hom.Error, vel=0 |
+      //       1      |       1      |         0      | Hom.Reserved |
+      //       1      |       1      |         1      | Hom.Reserved |
+      //-------------------------------------------------------------
+      case 0:
         LOG("Homing in Progress");
         break;
-      case 0b0000010000000000:
+      case (1<<SW_Target_reached):
         LOG("Homing not started");
         break;
-      case 0b0001000000000000:
+      case (1<<SW_Operation_specific0):
         LOG("Homing attained, target not reached");
         break;
-      case 0b0001010000000000:
+      case ((1<<SW_Operation_specific0) | (1<<SW_Target_reached)):
         homing_needed_ = false;
         LOG("Homing succesful");
         break;
-      case 0b0010000000000000:
+      case (1<<SW_Operation_specific1):
         LOG("Homing error , vel!=0");
+        status.error("Homing error, vel!=0");
         break;
-      case 0b0010010000000000:
-        LOG("Homing error, vel!=0");
+      case ((1<<SW_Operation_specific1) | (1<<SW_Target_reached)):
+        LOG("Homing error, vel=0");
+        status.error("Homing error, vel=0");
         break;
-      case 0b0011000000000000:
-      case 0b0011010000000000:
+      case (1<<SW_Operation_specific1 | 1<<SW_Operation_specific0):
+      case (1<<SW_Operation_specific1 | 1<<SW_Operation_specific0 | 1<<SW_Target_reached):
         LOG("Homing reserved");
         break;
       }
@@ -136,29 +205,45 @@ void Node_402::pending(LayerStatus &status)
   control_word.set(cw_set);
 }
 
-void Node_402::getDeviceState()
+void Node_402::getDeviceState(LayerStatus &status)
 {
   std::bitset<16> sw_new(status_word.get());
 
   status_word_bitset = sw_new;
 
-  switch((status_word_bitset & status_word_mask).to_ulong())
+  switch ((status_word_bitset & status_word_mask).to_ulong())
   {
   case 0b0000000:
-  case 0b0100000: state_ = Not_Ready_To_Switch_On; break;
+  case 0b0100000:
+    state_ = Not_Ready_To_Switch_On;
+    break;
   case 0b1000000:
-  case 0b1100000: state_ = Switch_On_Disabled; break;
-  case 0b0100001: state_ = Ready_To_Switch_On; break;
-  case 0b0100011: state_ = Switched_On; break;
-  case 0b0100111: state_ = Operation_Enable; break;
-  case 0b0000111: state_ = Quick_Stop_Active; break;
+  case 0b1100000:
+    state_ = Switch_On_Disabled;
+    break;
+  case 0b0100001:
+    state_ = Ready_To_Switch_On;
+    break;
+  case 0b0100011:
+    state_ = Switched_On;
+    break;
+  case 0b0100111:
+    state_ = Operation_Enable;
+    break;
+  case 0b0000111:
+    state_ = Quick_Stop_Active;
+    break;
   case 0b0001111:
-  case 0b0101111: state_ = Fault_Reaction_Active; break;
+  case 0b0101111:
+    state_ = Fault_Reaction_Active;
+    break;
   case 0b0001000:
-  case 0b0101000: state_ = Fault; break;
+  case 0b0101000:
+    state_ = Fault;
+    break;
   default:
     LOG("Motor currently in an unknown state");
-    //status.error();
+    status.error("Motor currently in an unknown state");
   }
 }
 
@@ -166,7 +251,7 @@ void Node_402::switchMode(LayerStatus &status)
 {
   target_pos_ = ac_pos_;
   target_vel_ = ac_vel_;
-  if(operation_mode_ == operation_mode_to_set_)
+  if (operation_mode_ == operation_mode_to_set_)
   {
     check_mode = false;
   }
@@ -189,28 +274,24 @@ bool Node_402::enterMode(const OperationMode &op_mode_var)
 
 void Node_402::read(LayerStatus &status)
 {
+  getDeviceState(status);
 
-  getDeviceState();
-
-  operation_mode_ = (OperationMode) op_mode_display.get(); // TODO: check validity
+  operation_mode_ = (OperationMode) op_mode_display.get();  // TODO(thiagodefreitas): check validity
   ac_vel_ = actual_vel.get();
   ac_pos_ = actual_pos.get();
-  oldpos_ = ac_pos_;//target_position.get_cached();
-  if(check_mode)
+  oldpos_ = ac_pos_;
+  if (check_mode)
   {
     switchMode(status);
   }
-
 }
 
 void Node_402::shutdown(LayerStatus &status)
 {
-
 }
 
 void Node_402::diag(LayerReport &report)
 {
-
 }
 
 void Node_402::halt(LayerStatus &status)
@@ -226,17 +307,16 @@ void Node_402::recover(LayerStatus &status)
 
   boost::mutex::scoped_lock cond_lock(cond_mutex);
 
-  while(!motor_ready_)
+  while (!motor_ready_)
   {
-    if(cond.wait_until(cond_lock,t0) == boost::cv_status::timeout)
+    if (cond.wait_until(cond_lock, t0) == boost::cv_status::timeout)
     {
-      if(!motor_ready_){
-        BOOST_THROW_EXCEPTION( TimeoutException() );
+      if (!motor_ready_)
+      {
+        BOOST_THROW_EXCEPTION(TimeoutException());
       }
     }
   }
-
-
 }
 
 const double Node_402::getTargetPos()
@@ -347,19 +427,19 @@ void Node_402::motorFaultReset()
 
 void Node_402::write(LayerStatus &status)
 {
-  if(state_ == Operation_Enable)
+  if (state_ == Operation_Enable)
   {
-    switch(operation_mode_)
+    switch (operation_mode_)
     {
     case Profiled_Position:
-      if(new_target_pos_)
+      if (new_target_pos_)
       {
         control_word_bitset.reset(CW_Operation_mode_specific0);
         control_word_bitset.reset(CW_Operation_mode_specific1);
         control_word_bitset.reset(CW_Operation_mode_specific2);
         new_target_pos_ = false;
       }
-      else if(oldpos_ != target_pos_)
+      else if (oldpos_ != target_pos_)
       {
         target_position.set(target_pos_);
         new_target_pos_ = true;
@@ -376,7 +456,7 @@ void Node_402::write(LayerStatus &status)
       break;
     case Interpolated_Position:
       target_interpolated_position.set(target_pos_);
-      if(ip_mode_sub_mode.get_cached() == -1)
+      if (ip_mode_sub_mode.get_cached() == -1)
         target_interpolated_velocity.set(target_vel_);
       control_word_bitset.set(CW_Operation_mode_specific0);
       control_word_bitset.reset(CW_Operation_mode_specific1);
@@ -388,7 +468,6 @@ void Node_402::write(LayerStatus &status)
       control_word_bitset.set(CW_Operation_mode_specific1);
       control_word_bitset.set(CW_Operation_mode_specific2);
       break;
-
     }
   }
   int16_t cw_set = static_cast<int>(control_word_bitset.to_ulong());
@@ -411,9 +490,9 @@ bool Node_402::isModeSupported(const OperationMode &op_mode)
 }
 uint32_t Node_402::getModeMask(const OperationMode &op_mode)
 {
-  SupportedOperationMode sup_mode = static_cast<SupportedOperationMode>((op_mode)-1);
+  SupportedOperationMode sup_mode = static_cast<SupportedOperationMode>((op_mode) - 1);
 
-  switch(op_mode)
+  switch (op_mode)
   {
   case Sup_Profiled_Position:
   case Sup_Velocity:
@@ -424,7 +503,7 @@ uint32_t Node_402::getModeMask(const OperationMode &op_mode)
   case Sup_Cyclic_Synchronous_Velocity:
   case Sup_Cyclic_Synchronous_Torque:
   case Sup_Homing:
-    return (1<<(op_mode-1));
+    return (1 << (op_mode - 1));
   case Sup_Reserved:
     return 0;
   }
@@ -473,42 +552,42 @@ void Node_402::configureEntries()
   n_->getStorage()->entry(status_word, 0x6041);
   n_->getStorage()->entry(control_word, 0x6040);
 
-  n_->getStorage()->entry(op_mode,0x6060);
-  n_->getStorage()->entry(op_mode_display,0x6061);
-  n_->getStorage()->entry(supported_drive_modes,0x6502);
+  n_->getStorage()->entry(op_mode, 0x6060);
+  n_->getStorage()->entry(op_mode_display, 0x6061);
+  n_->getStorage()->entry(supported_drive_modes, 0x6502);
 
-  n_->getStorage()->entry(ip_mode_sub_mode,0x60C0);
+  n_->getStorage()->entry(ip_mode_sub_mode, 0x60C0);
 
-  n_->getStorage()->entry(actual_vel,0x606C);
+  n_->getStorage()->entry(actual_vel, 0x606C);
 
-  n_->getStorage()->entry(actual_pos,0x6064);
+  n_->getStorage()->entry(actual_pos, 0x6064);
 }
 
 void Node_402::configureModeSpecificEntries()
 {
-  if(isModeSupported(Profiled_Position))
+  if (isModeSupported(Profiled_Position))
   {
-    n_->getStorage()->entry(target_position,0x607A);
-    n_->getStorage()->entry(profile_velocity,0x6081);
+    n_->getStorage()->entry(target_position, 0x607A);
+    n_->getStorage()->entry(profile_velocity, 0x6081);
   }
-  if(isModeSupported(Profiled_Velocity))
+  if (isModeSupported(Profiled_Velocity))
   {
-    n_->getStorage()->entry(target_profiled_velocity,0x60FF);
+    n_->getStorage()->entry(target_profiled_velocity, 0x60FF);
   }
-  if(isModeSupported(Interpolated_Position))
+  if (isModeSupported(Interpolated_Position))
   {
-    n_->getStorage()->entry(target_interpolated_position,0x60C1,0x01);
-    if(ip_mode_sub_mode.get_cached() == -1)
-      n_->getStorage()->entry(target_interpolated_velocity,0x60C1,0x02);
+    n_->getStorage()->entry(target_interpolated_position, 0x60C1, 0x01);
+    if (ip_mode_sub_mode.get_cached() == -1)
+      n_->getStorage()->entry(target_interpolated_velocity, 0x60C1, 0x02);
   }
-  if(isModeSupported(Velocity))
+  if (isModeSupported(Velocity))
   {
-    n_->getStorage()->entry(target_velocity,0x6042);
+    n_->getStorage()->entry(target_velocity, 0x6042);
   }
 
-  if(isModeSupported(Homing))
+  if (isModeSupported(Homing))
   {
-    n_->getStorage()->entry(homing_method,0x6098);
+    n_->getStorage()->entry(homing_method, 0x6098);
   }
 }
 
@@ -534,24 +613,24 @@ void Node_402::init(LayerStatus &s)
 
   Node_402::configureModeSpecificEntries();
 
-  if(homing_method.get() != 0)
+  if (homing_method.get() != 0)
     homing_needed_ = true;
 
-  if(Node_402::turnOn())
+  if (Node_402::turnOn())
   {
-    running=true;
+    running = true;
   }
   else
     s.error();
 
-  while(!motor_ready_)
+  while (!motor_ready_)
   {
-    if(cond.wait_until(cond_lock,t0) == boost::cv_status::timeout)
+    if (cond.wait_until(cond_lock, t0) == boost::cv_status::timeout)
     {
-      if(!motor_ready_){
-        BOOST_THROW_EXCEPTION( TimeoutException() );
+      if (!motor_ready_)
+      {
+        BOOST_THROW_EXCEPTION(TimeoutException());
       }
     }
   }
-
 }
