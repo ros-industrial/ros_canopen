ee886c078b7011445e31e8dc50eb9cff4810a33f
thiagodefreitas <thiagodefreitas@gmail.com>
2015-02-26 17:13:39 +0100

    Test file

--- canopen_402/src/canopen_402/test.cpp
+++ canopen_402/src/canopen_402/test.cpp
@@ -1,1 +1,18 @@
+#include <canopen_402/canopen_402.h>
+
+using namespace can;
+using namespace canopen;
+
+using canopen::Node_402;
+
+int main()
+{
+  std::string name = "1234";
+  boost::shared_ptr<Node_402> motor( new Node_402(name));
+  LayerStatus s;
+
+  motor->init(s);
+
+  return 0;
+}
 
