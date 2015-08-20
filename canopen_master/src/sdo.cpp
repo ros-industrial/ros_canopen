8c6387fb2ae3c2e6b7d3f39cf1e9ccda6aa549b6
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2015-08-20 08:16:57 +0000

    removed unnecessary sleep (added for tests only)

--- canopen_master/src/sdo.cpp
+++ canopen_master/src/sdo.cpp
@@ -414,7 +414,6 @@ void SDOClient::transmitAndWait(const canopen::ObjectDict::Entry &entry, const S
     can::Frame msg;
 
     while(!done){
-        boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
         if(!reader_.read(&msg,boost::chrono::seconds(1)))
         {
             abort(0x05040000); // SDO protocol timed out.
