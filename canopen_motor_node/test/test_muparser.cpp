782cdbac795485f853cb582c397c49a907689f77
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-02-10 13:18:22 +0100

    added test for norm function

---
+++ canopen_motor_node/test/test_muparser.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <canopen_motor_node/robot_layer.h>
+#include <boost/bind.hpp>
+
+double * mapVariable(const std::string &, double *p) {
+    return p;
+}
+
+TEST(TestMuparser, CheckNorm){
+    double input = 0;
+    UnitConverter uc("norm(in,-1000,1000)", boost::bind(HandleLayer::assignVariable, _1, &input, "in"));
+    input = 0; EXPECT_EQ(0, uc.evaluate());
+    input = 10; EXPECT_EQ(10, uc.evaluate());
+    input = -10; EXPECT_EQ(-10, uc.evaluate());
+    input = 1000; EXPECT_EQ(-1000, uc.evaluate());
+    input = 1001; EXPECT_EQ(-999, uc.evaluate());
+    input = 2000; EXPECT_EQ(0, uc.evaluate());
+    input = 2001; EXPECT_EQ(1, uc.evaluate());
+    input = -1000; EXPECT_EQ(-1000, uc.evaluate());
+    input = 999; EXPECT_EQ(999, uc.evaluate());
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
