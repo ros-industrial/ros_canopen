03798f7a5bbae34bda2a962e11084c16f3e39abe
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-09-22 19:31:32 +0200

    added test for FilteredFrameListener

--- socketcan_interface/test/test_filter.cpp
+++ socketcan_interface/test/test_filter.cpp
@@ -3,6 +3,7 @@
 
 
 #include <socketcan_interface/string.h>
+#include <socketcan_interface/dummy.h>
 
 // Bring in gtest
 #include <gtest/gtest.h>
@@ -65,6 +66,39 @@ TEST(FilterTest, rangeTest)
   EXPECT_TRUE(f3->pass(can::toframe(msg1)));
   EXPECT_TRUE(f3->pass(can::toframe(msg2)));
   EXPECT_FALSE(f3->pass(can::toframe(msg3)));
+
+}
+
+class Counter {
+public:
+    size_t count_;
+    Counter(): count_(0) {}
+    void count(const can::Frame &frame) {
+      ++count_;
+    }
+};
+
+TEST(FilterTest, listenerTest)
+{
+
+  Counter counter;
+  boost::shared_ptr<can::CommInterface> dummy(new can::DummyInterface(true));
+
+  can::FilteredFrameListener::FilterVector filters;
+  filters.push_back(can::tofilter("123:FFE"));
+
+  can::CommInterface::FrameListener::Ptr  listener(new can::FilteredFrameListener(dummy,can::CommInterface::FrameDelegate(&counter, &Counter::count), filters));
+
+  can::Frame f1 = can::toframe("123#");
+  can::Frame f2 = can::toframe("124#");
+  can::Frame f3 = can::toframe("122#");
+
+  dummy->send(f1);
+  EXPECT_EQ(1, counter.count_);
+  dummy->send(f2);
+  EXPECT_EQ(1, counter.count_);
+  dummy->send(f3);
+  EXPECT_EQ(2, counter.count_);
 
 }
 
