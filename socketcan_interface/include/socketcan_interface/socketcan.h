d11f53725f29ab8a048add5ee6987bc31f3602b2
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-02-11 21:45:14 -0600

    Linter and other fixes.

--- socketcan_interface/include/socketcan_interface/socketcan.h
+++ socketcan_interface/include/socketcan_interface/socketcan.h
@@ -1,5 +1,22 @@
-#ifndef H_SOCKETCAN_DRIVER
-#define H_SOCKETCAN_DRIVER
+/*
+ * Copyright 2019 Fraunhofer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SOCKETCAN_INTERFACE_SOCKETCAN_H
+#define SOCKETCAN_INTERFACE_SOCKETCAN_H
 
 #include <socketcan_interface/asio_base.h>
 #include <boost/bind.hpp>
@@ -230,5 +247,6 @@ using ThreadedSocketCANInterface = ThreadedInterface<SocketCANInterface>;
 using ThreadedSocketCANInterfaceSharedPtr = std::shared_ptr<ThreadedSocketCANInterface>;
 
 
-} // namespace can
-#endif
+}  // namespace can
+
+#endif  // SOCKETCAN_INTERFACE_SOCKETCAN_H
--- socketcan_interface/include/socketcan_interface/string.h
+++ socketcan_interface/include/socketcan_interface/string.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019 Fraunhofer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SOCKETCAN_INTERFACE_STRING_H
 #define SOCKETCAN_INTERFACE_STRING_H
 
@@ -40,8 +57,9 @@ template <typename T> FilteredFrameListener::FilterVector tofilters(const T& v) 
     return filters;
 }
 
-std::ostream& operator <<(std::ostream& stream, const Header& h);
-std::ostream& operator <<(std::ostream& stream, const Frame& f);
+}  // namespace can
 
-}
-#endif
+std::ostream& operator <<(std::ostream& stream, const can::Header& h);
+std::ostream& operator <<(std::ostream& stream, const can::Frame& f);
+
+#endif  // SOCKETCAN_INTERFACE_STRING_H
