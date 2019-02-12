4c057e252d1c05856ddf3b9dfbc481846b1dc0e2
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-02-11 21:45:14 -0600

    Linter and other fixes.

--- socketcan_interface/include/socketcan_interface/make_shared.h
+++ socketcan_interface/include/socketcan_interface/make_shared.h
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2019 Fraunhofer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SOCKETCAN_INTERFACE_MAKE_SHARED_H
 #define SOCKETCAN_INTERFACE_MAKE_SHARED_H
 
 #include <memory>
 #define ROSCANOPEN_MAKE_SHARED std::make_shared
 
-#endif // ! SOCKETCAN_INTERFACE_MAKE_SHARED_H
+#endif  // SOCKETCAN_INTERFACE_MAKE_SHARED_H
--- socketcan_interface/include/socketcan_interface/reader.h
+++ socketcan_interface/include/socketcan_interface/reader.h
@@ -1,5 +1,22 @@
-#ifndef H_CAN_BUFFERED_READER
-#define H_CAN_BUFFERED_READER
+/*
+ * Copyright 2019 Fraunhofer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SOCKETCAN_INTERFACE_READER_H
+#define SOCKETCAN_INTERFACE_READER_H
 
 #include <socketcan_interface/interface.h>
 #include <deque>
@@ -111,4 +128,5 @@ public:
 };
 
 } // namespace can
-#endif
+
+#endif  // SOCKETCAN_INTERFACE_READER_H
