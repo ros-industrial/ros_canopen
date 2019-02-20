00c9637b6d9274aba2e063238db1690d26788a01
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-02-19 22:56:32 -0600

    Adding license headers to socketcan_interface.

--- socketcan_interface/include/socketcan_interface/bcm.hpp
+++ socketcan_interface/include/socketcan_interface/bcm.hpp
@@ -1,19 +1,17 @@
-/*
- * Copyright 2019 Fraunhofer
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright (c) 2016-2019, Fraunhofer, Mathias Lüdtke, AutonomouStuff
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef SOCKETCAN_INTERFACE__BCM_HPP_
 #define SOCKETCAN_INTERFACE__BCM_HPP_
--- socketcan_interface/test/test_dummy_interface.cpp
+++ socketcan_interface/test/test_dummy_interface.cpp
@@ -1,4 +1,18 @@
-// Bring in my package's API, which is what I'm testing
+// Copyright (c) 2016-2019, Mathias Lüdtke, AutonomouStuff
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "socketcan_interface/dispatcher.hpp"
 #include "socketcan_interface/dummy.hpp"
 
