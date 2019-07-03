686e64f95879f3eff4ac4b0bea130d5d32ef2bc6
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-07-03 14:26:09 +0200

    no need for local variable

--- socketcan_bridge/src/socketcan_to_topic.cpp
+++ socketcan_bridge/src/socketcan_to_topic.cpp
@@ -48,9 +48,7 @@ namespace socketcan_bridge
   SocketCANToTopic::SocketCANToTopic(ros::NodeHandle* nh, ros::NodeHandle* nh_param,
       can::DriverInterfaceSharedPtr driver)
     {
-      int queue_size;
-      nh_param->param<int>("received_messages_queue_size", queue_size, 10);
-      can_topic_ = nh->advertise<can_msgs::Frame>("received_messages", queue_size);
+      can_topic_ = nh->advertise<can_msgs::Frame>("received_messages", nh_param->param("received_messages_queue_size", 10));
       driver_ = driver;
     };
 
