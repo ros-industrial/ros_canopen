ros-industrial/socketcan_interface

socketcan_interface - fixing whitespace.

socketcan_interface/include/socketcan_interface/asio_base.h
@@ -1,16 +1,16 @@
 /*
  * Copyright 2019 Fraunhofer
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,123 +25,152 @@
 #include <boost/thread/thread.hpp>
 #include <functional>
 
-namespace can{
+namespace can
+{
 
 
-template<typename Socket> class AsioDriver : public DriverInterface{
-    typedef FilteredDispatcher<unsigned int, CommInterface::FrameListener> FrameDispatcher;
-    typedef SimpleDispatcher<StateInterface::StateListener> StateDispatcher;
-    FrameDispatcher frame_dispatcher_;
-    StateDispatcher state_dispatcher_;
+template<typename Socket> class AsioDriver : public DriverInterface
+{
+  typedef FilteredDispatcher<unsigned int, CommInterface::FrameListener> FrameDispatcher;
+  typedef SimpleDispatcher<StateInterface::StateListener> StateDispatcher;
+  FrameDispatcher frame_dispatcher_;
+  StateDispatcher state_dispatcher_;
 
-    State state_;
-    boost::mutex state_mutex_;
-    boost::mutex socket_mutex_;
+  State state_;
+  boost::mutex state_mutex_;
+  boost::mutex socket_mutex_;
 
 protected:
-    boost::asio::io_service io_service_;
+  boost::asio::io_service io_service_;
 #if BOOST_ASIO_VERSION >= 101200 // Boost 1.66+
-    boost::asio::io_context::strand strand_;
+  boost::asio::io_context::strand strand_;
 #else
-    boost::asio::strand strand_;
+  boost::asio::strand strand_;
 #endif
-    Socket socket_;
-    Frame input_;
+  Socket socket_;
+  Frame input_;
 
-    virtual void triggerReadSome() = 0;
-    virtual bool enqueue(const Frame & msg) = 0;
+  virtual void triggerReadSome() = 0;
+  virtual bool enqueue(const Frame & msg) = 0;
 
-    void dispatchFrame(const Frame &msg){
-        strand_.post(boost::bind(&FrameDispatcher::dispatch, &frame_dispatcher_, msg)); // copies msg
+  void dispatchFrame(const Frame &msg)
+  {
+    strand_.post(boost::bind(&FrameDispatcher::dispatch, &frame_dispatcher_, msg)); // copies msg
+  }
+  void setErrorCode(const boost::system::error_code& error)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.error_code != error)
+    {
+      state_.error_code = error;
+      state_dispatcher_.dispatch(state_);
     }
-    void setErrorCode(const boost::system::error_code& error){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.error_code != error){
-            state_.error_code = error;
-            state_dispatcher_.dispatch(state_);
-        }
+  }
+  void setInternalError(unsigned int internal_error)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.internal_error != internal_error)
+    {
+      state_.internal_error = internal_error;
+      state_dispatcher_.dispatch(state_);
     }
-    void setInternalError(unsigned int internal_error){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.internal_error != internal_error){
-            state_.internal_error = internal_error;
-            state_dispatcher_.dispatch(state_);
-        }
-    }
+  }
 
-    void setDriverState(State::DriverState state){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.driver_state != state){
-            state_.driver_state = state;
-            state_dispatcher_.dispatch(state_);
-        }
+  void setDriverState(State::DriverState state)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.driver_state != state)
+    {
+      state_.driver_state = state;
+      state_dispatcher_.dispatch(state_);
     }
-    void setNotReady(){
-        setDriverState(socket_.is_open()?State::open : State::closed);
-    }
+  }
+  void setNotReady()
+  {
+    setDriverState(socket_.is_open() ? State::open : State::closed);
+  }
 
-    void frameReceived(const boost::system::error_code& error){
-        if(!error){
-            dispatchFrame(input_);
-            triggerReadSome();
-        }else{
-            setErrorCode(error);
-            setNotReady();
-        }
+  void frameReceived(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      dispatchFrame(input_);
+      triggerReadSome();
     }
+    else
+    {
+      setErrorCode(error);
+      setNotReady();
+    }
+  }
 
-    AsioDriver()
+  AsioDriver()
     : strand_(io_service_), socket_(io_service_)
-    {}
+  {}
 
 public:
-    virtual ~AsioDriver() { shutdown(); }
+  virtual ~AsioDriver()
+  {
+    shutdown();
+  }
 
-    State getState(){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        return state_;
+  State getState()
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    return state_;
+  }
+  virtual void run()
+  {
+    setNotReady();
+
+    if (getState().driver_state == State::open)
+    {
+      io_service_.reset();
+      boost::asio::io_service::work work(io_service_);
+      setDriverState(State::ready);
+
+      boost::thread post_thread([this]()
+      {
+        io_service_.run();
+      });
+
+      triggerReadSome();
+
+      boost::system::error_code ec;
+      io_service_.run(ec);
+      setErrorCode(ec);
+
+      setNotReady();
     }
-    virtual void run(){
-        setNotReady();
+    state_dispatcher_.dispatch(getState());
+  }
+  virtual bool send(const Frame & msg)
+  {
+    return getState().driver_state == State::ready && enqueue(msg);
+  }
 
-        if(getState().driver_state == State::open){
-            io_service_.reset();
-            boost::asio::io_service::work work(io_service_);
-            setDriverState(State::ready);
-
-            boost::thread post_thread([this]() { io_service_.run(); });
-
-            triggerReadSome();
-
-            boost::system::error_code ec;
-            io_service_.run(ec);
-            setErrorCode(ec);
-
-            setNotReady();
-        }
-        state_dispatcher_.dispatch(getState());
+  virtual void shutdown()
+  {
+    if (socket_.is_open())
+    {
+      socket_.cancel();
+      socket_.close();
     }
-    virtual bool send(const Frame & msg){
-        return getState().driver_state == State::ready && enqueue(msg);
-    }
+    io_service_.stop();
+  }
 
-    virtual void shutdown(){
-        if(socket_.is_open()){
-            socket_.cancel();
-            socket_.close();
-        }
-        io_service_.stop();
-    }
-
-    virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(delegate);
-    }
-    virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h , const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(h, delegate);
-    }
-    virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate){
-        return state_dispatcher_.createListener(delegate);
-    }
+  virtual FrameListenerConstSharedPtr createMsgListener(const FrameDelegate &delegate)
+  {
+    return frame_dispatcher_.createListener(delegate);
+  }
+  virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h, const FrameDelegate &delegate)
+  {
+    return frame_dispatcher_.createListener(h, delegate);
+  }
+  virtual StateListenerConstSharedPtr createStateListener(const StateDelegate &delegate)
+  {
+    return state_dispatcher_.createListener(delegate);
+  }
 
 };
 

socketcan_interface/include/socketcan_interface/dummy.h
@@ -1,16 +1,16 @@
 /*
  * Copyright 2019 Fraunhofer
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,90 +25,117 @@
 #include "string.h"
 #include <boost/algorithm/string.hpp>
 
-namespace can{
+namespace can
+{
 
-class DummyInterface : public DriverInterface{
-    typedef FilteredDispatcher<unsigned int, CommInterface::FrameListener> FrameDispatcher;
-    typedef SimpleDispatcher<StateInterface::StateListener> StateDispatcher;
-    typedef std::unordered_multimap<std::string, Frame> Map;
-    FrameDispatcher frame_dispatcher_;
-    StateDispatcher state_dispatcher_;
-    State state_;
-    Map map_;
-    bool loopback_;
+class DummyInterface : public DriverInterface
+{
+  typedef FilteredDispatcher<unsigned int, CommInterface::FrameListener> FrameDispatcher;
+  typedef SimpleDispatcher<StateInterface::StateListener> StateDispatcher;
+  typedef std::unordered_multimap<std::string, Frame> Map;
+  FrameDispatcher frame_dispatcher_;
+  StateDispatcher state_dispatcher_;
+  State state_;
+  Map map_;
+  bool loopback_;
 
-    bool add_noconv(const std::string &k, const Frame &v, bool multi){
-        if(multi || map_.find(k) == map_.end()){
-              map_.insert( std::make_pair(boost::to_lower_copy(k), v));
-              return true;
-        }
-        return false;
+  bool add_noconv(const std::string &k, const Frame &v, bool multi)
+  {
+    if (multi || map_.find(k) == map_.end())
+    {
+      map_.insert(std::make_pair(boost::to_lower_copy(k), v));
+      return true;
     }
+    return false;
+  }
 public:
-    DummyInterface(bool loopback) : loopback_(loopback) {}
+  DummyInterface(bool loopback) : loopback_(loopback) {}
 
-    bool add(const std::string &k, const Frame &v, bool multi){
-        return add_noconv(boost::to_lower_copy(k), v, multi);
+  bool add(const std::string &k, const Frame &v, bool multi)
+  {
+    return add_noconv(boost::to_lower_copy(k), v, multi);
+  }
+  bool add(const Frame &k, const Frame &v, bool multi)
+  {
+    return add_noconv(tostring(k, true), v, multi);
+  }
+  bool add(const std::string &k, const std::string &v, bool multi)
+  {
+    return add(k, toframe(v), multi);
+  }
+  bool add(const Frame &k, const std::string &v, bool multi)
+  {
+    return add(k, toframe(v), multi);
+  }
+  virtual bool send(const Frame & msg)
+  {
+    if (loopback_) frame_dispatcher_.dispatch(msg);
+    try
+    {
+      std::pair <Map::iterator, Map::iterator> r = map_.equal_range(tostring(msg, true));
+      for (Map::iterator it = r.first; it != r.second; ++it)
+      {
+        frame_dispatcher_.dispatch(it->second);
+      }
     }
-    bool add(const Frame &k, const Frame &v, bool multi){
-        return add_noconv(tostring(k,true), v, multi);
+    catch (const std::out_of_range &e)
+    {
     }
-    bool add(const std::string &k, const std::string &v, bool multi){
-        return add(k, toframe(v), multi);
+    return true;
+  }
+
+  virtual FrameListenerConstSharedPtr createMsgListener(const FrameDelegate &delegate)
+  {
+    return frame_dispatcher_.createListener(delegate);
+  }
+  virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h, const FrameDelegate &delegate)
+  {
+    return frame_dispatcher_.createListener(h, delegate);
+  }
+
+  // methods from StateInterface
+  virtual bool recover()
+  {
+    return false;
+  };
+
+  virtual State getState()
+  {
+    return state_;
+  };
+
+  virtual void shutdown() {};
+
+  virtual bool translateError(unsigned int internal_error, std::string & str)
+  {
+    if (!internal_error)
+    {
+      str = "OK";
+      return true;
     }
-    bool add(const Frame &k, const std::string &v, bool multi){
-        return add(k, toframe(v), multi);
-    }
-    virtual bool send(const Frame & msg){
-        if(loopback_) frame_dispatcher_.dispatch(msg);
-        try{
-            std::pair <Map::iterator, Map::iterator> r = map_.equal_range(tostring(msg, true));
-            for (Map::iterator it=r.first; it!=r.second; ++it){
-                frame_dispatcher_.dispatch(it->second);
-            }
-        }
-        catch(const std::out_of_range &e){
-        }
-        return true;
-    }
+    return false;
+  };
 
-    virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(delegate);
-    }
-    virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h , const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(h, delegate);
-    }
+  virtual bool doesLoopBack() const
+  {
+    return loopback_;
+  };
 
-    // methods from StateInterface
-    virtual bool recover(){return false;};
+  virtual void run() {};
 
-    virtual State getState(){return state_;};
+  bool init(const std::string &device, bool loopback)
+  {
+    loopback_ = loopback;
+    state_.driver_state = State::ready;
+    state_.internal_error = 0;
+    state_dispatcher_.dispatch(state_);
+    return true;
+  };
 
-    virtual void shutdown(){};
-
-    virtual bool translateError(unsigned int internal_error, std::string & str){
-        if (!internal_error) {
-            str = "OK";
-            return true;
-        }
-        return false;
-    };
-
-    virtual bool doesLoopBack() const {return loopback_;};
-
-    virtual void run(){};
-
-    bool init(const std::string &device, bool loopback){
-        loopback_ = loopback;
-        state_.driver_state = State::ready;
-        state_.internal_error = 0;
-        state_dispatcher_.dispatch(state_);
-        return true;
-    };
-
-    virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate){
-      return state_dispatcher_.createListener(delegate);
-    };
+  virtual StateListenerConstSharedPtr createStateListener(const StateDelegate &delegate)
+  {
+    return state_dispatcher_.createListener(delegate);
+  };
 
 };
 typedef std::shared_ptr<DummyInterface> DummyInterfaceSharedPtr;

ros-industrial/ros_canopen

socketcan_interface/include/socketcan_interface/asio_base.h
@@ -1,16 +1,16 @@
 /*
  * Copyright 2019 Fraunhofer
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,123 +25,152 @@
 #include <boost/thread/thread.hpp>
 #include <functional>
 
-namespace can{
+namespace can
+{
 
 
-template<typename Socket> class AsioDriver : public DriverInterface{
-    using FrameDispatcher = FilteredDispatcher<unsigned int, CommInterface::FrameListener>;
-    using StateDispatcher = SimpleDispatcher<StateInterface::StateListener>;
-    FrameDispatcher frame_dispatcher_;
-    StateDispatcher state_dispatcher_;
+template<typename Socket> class AsioDriver : public DriverInterface
+{
+  using FrameDispatcher = FilteredDispatcher<unsigned int, CommInterface::FrameListener>;
+  using StateDispatcher = SimpleDispatcher<StateInterface::StateListener>;
+  FrameDispatcher frame_dispatcher_;
+  StateDispatcher state_dispatcher_;
 
-    State state_;
-    boost::mutex state_mutex_;
-    boost::mutex socket_mutex_;
+  State state_;
+  boost::mutex state_mutex_;
+  boost::mutex socket_mutex_;
 
 protected:
-    boost::asio::io_service io_service_;
+  boost::asio::io_service io_service_;
 #if BOOST_ASIO_VERSION >= 101200 // Boost 1.66+
-    boost::asio::io_context::strand strand_;
+  boost::asio::io_context::strand strand_;
 #else
-    boost::asio::strand strand_;
+  boost::asio::strand strand_;
 #endif
-    Socket socket_;
-    Frame input_;
+  Socket socket_;
+  Frame input_;
 
-    virtual void triggerReadSome() = 0;
-    virtual bool enqueue(const Frame & msg) = 0;
+  virtual void triggerReadSome() = 0;
+  virtual bool enqueue(const Frame & msg) = 0;
 
-    void dispatchFrame(const Frame &msg){
-        strand_.post([this, msg]{ frame_dispatcher_.dispatch(msg.key(), msg);} ); // copies msg
+  void dispatchFrame(const Frame &msg)
+  {
+    strand_.post([this, msg]{ frame_dispatcher_.dispatch(msg.key(), msg);} ); // copies msg
+  }
+  void setErrorCode(const boost::system::error_code& error)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.error_code != error)
+    {
+      state_.error_code = error;
+      state_dispatcher_.dispatch(state_);
     }
-    void setErrorCode(const boost::system::error_code& error){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.error_code != error){
-            state_.error_code = error;
-            state_dispatcher_.dispatch(state_);
-        }
+  }
+  void setInternalError(unsigned int internal_error)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.internal_error != internal_error)
+    {
+      state_.internal_error = internal_error;
+      state_dispatcher_.dispatch(state_);
     }
-    void setInternalError(unsigned int internal_error){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.internal_error != internal_error){
-            state_.internal_error = internal_error;
-            state_dispatcher_.dispatch(state_);
-        }
-    }
+  }
 
-    void setDriverState(State::DriverState state){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        if(state_.driver_state != state){
-            state_.driver_state = state;
-            state_dispatcher_.dispatch(state_);
-        }
+  void setDriverState(State::DriverState state)
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    if (state_.driver_state != state)
+    {
+      state_.driver_state = state;
+      state_dispatcher_.dispatch(state_);
     }
-    void setNotReady(){
-        setDriverState(socket_.is_open()?State::open : State::closed);
-    }
+  }
+  void setNotReady()
+  {
+    setDriverState(socket_.is_open() ? State::open : State::closed);
+  }
 
-    void frameReceived(const boost::system::error_code& error){
-        if(!error){
-            dispatchFrame(input_);
-            triggerReadSome();
-        }else{
-            setErrorCode(error);
-            setNotReady();
-        }
+  void frameReceived(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      dispatchFrame(input_);
+      triggerReadSome();
     }
+    else
+    {
+      setErrorCode(error);
+      setNotReady();
+    }
+  }
 
-    AsioDriver()
+  AsioDriver()
     : strand_(io_service_), socket_(io_service_)
-    {}
+  {}
 
 public:
-    virtual ~AsioDriver() { shutdown(); }
+  virtual ~AsioDriver()
+  {
+    shutdown();
+  }
 
-    State getState(){
-        boost::mutex::scoped_lock lock(state_mutex_);
-        return state_;
+  State getState()
+  {
+    boost::mutex::scoped_lock lock(state_mutex_);
+    return state_;
+  }
+  virtual void run()
+  {
+    setNotReady();
+
+    if (getState().driver_state == State::open)
+    {
+      io_service_.reset();
+      boost::asio::io_service::work work(io_service_);
+      setDriverState(State::ready);
+
+      boost::thread post_thread([this]()
+      {
+        io_service_.run();
+      });
+
+      triggerReadSome();
+
+      boost::system::error_code ec;
+      io_service_.run(ec);
+      setErrorCode(ec);
+
+      setNotReady();
     }
-    virtual void run(){
-        setNotReady();
+    state_dispatcher_.dispatch(getState());
+  }
+  virtual bool send(const Frame & msg)
+  {
+    return getState().driver_state == State::ready && enqueue(msg);
+  }
 
-        if(getState().driver_state == State::open){
-            io_service_.reset();
-            boost::asio::io_service::work work(io_service_);
-            setDriverState(State::ready);
-
-            boost::thread post_thread([this]() { io_service_.run(); });
-
-            triggerReadSome();
-
-            boost::system::error_code ec;
-            io_service_.run(ec);
-            setErrorCode(ec);
-
-            setNotReady();
-        }
-        state_dispatcher_.dispatch(getState());
+  virtual void shutdown()
+  {
+    if (socket_.is_open())
+    {
+      socket_.cancel();
+      socket_.close();
     }
-    virtual bool send(const Frame & msg){
-        return getState().driver_state == State::ready && enqueue(msg);
-    }
+    io_service_.stop();
+  }
 
-    virtual void shutdown(){
-        if(socket_.is_open()){
-            socket_.cancel();
-            socket_.close();
-        }
-        io_service_.stop();
-    }
-
-    virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(delegate);
-    }
-    virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h , const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(h.key(), delegate);
-    }
-    virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate){
-        return state_dispatcher_.createListener(delegate);
-    }
+  virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate)
+  {
+    return frame_dispatcher_.createListener(delegate);
+  }
+  virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h , const FrameFunc &delegate)
+  {
+    return frame_dispatcher_.createListener(h.key(), delegate);
+  }
+  virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate)
+  {
+    return state_dispatcher_.createListener(delegate);
+  }
 
 };
 

socketcan_interface/include/socketcan_interface/dummy.h
@@ -1,16 +1,16 @@
 /*
  * Copyright 2019 Fraunhofer
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,90 +25,117 @@
 #include "string.h"
 #include <boost/algorithm/string.hpp>
 
-namespace can{
+namespace can
+{
 
-class DummyInterface : public DriverInterface{
-    using FrameDispatcher = FilteredDispatcher<unsigned int, CommInterface::FrameListener>;
-    using StateDispatcher = SimpleDispatcher<StateInterface::StateListener>;
-    using Map = std::unordered_map<std::string, Frame>;
-    FrameDispatcher frame_dispatcher_;
-    StateDispatcher state_dispatcher_;
-    State state_;
-    Map map_;
-    bool loopback_;
+class DummyInterface : public DriverInterface
+{
+  using FrameDispatcher = FilteredDispatcher<unsigned int, CommInterface::FrameListener>;
+  using StateDispatcher = SimpleDispatcher<StateInterface::StateListener>;
+  using Map = std::unordered_map<std::string, Frame>;
+  FrameDispatcher frame_dispatcher_;
+  StateDispatcher state_dispatcher_;
+  State state_;
+  Map map_;
+  bool loopback_;
 
-    bool add_noconv(const std::string &k, const Frame &v, bool multi){
-        if(multi || map_.find(k) == map_.end()){
-              map_.insert( std::make_pair(boost::to_lower_copy(k), v));
-              return true;
-        }
-        return false;
+  bool add_noconv(const std::string &k, const Frame &v, bool multi)
+  {
+    if (multi || map_.find(k) == map_.end())
+    {
+      map_.insert(std::make_pair(boost::to_lower_copy(k), v));
+      return true;
     }
+    return false;
+  }
 public:
-    DummyInterface(bool loopback) : loopback_(loopback) {}
+  DummyInterface(bool loopback) : loopback_(loopback) {}
 
-    bool add(const std::string &k, const Frame &v, bool multi){
-        return add_noconv(boost::to_lower_copy(k), v, multi);
+  bool add(const std::string &k, const Frame &v, bool multi)
+  {
+    return add_noconv(boost::to_lower_copy(k), v, multi);
+  }
+  bool add(const Frame &k, const Frame &v, bool multi)
+  {
+    return add_noconv(tostring(k, true), v, multi);
+  }
+  bool add(const std::string &k, const std::string &v, bool multi)
+  {
+    return add(k, toframe(v), multi);
+  }
+  bool add(const Frame &k, const std::string &v, bool multi)
+  {
+    return add(k, toframe(v), multi);
+  }
+  virtual bool send(const Frame & msg)
+  {
+    if (loopback_) frame_dispatcher_.dispatch(msg);
+    try
+    {
+      std::pair <Map::iterator, Map::iterator> r = map_.equal_range(tostring(msg, true));
+      for (Map::iterator it = r.first; it != r.second; ++it)
+      {
+        frame_dispatcher_.dispatch(it->second);
+      }
     }
-    bool add(const Frame &k, const Frame &v, bool multi){
-        return add_noconv(tostring(k,true), v, multi);
+    catch (const std::out_of_range &e)
+    {
     }
-    bool add(const std::string &k, const std::string &v, bool multi){
-        return add(k, toframe(v), multi);
+    return true;
+  }
+
+  virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate)
+  {
+    return frame_dispatcher_.createListener(delegate);
+  }
+  virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h, const FrameFunc &delegate)
+  {
+    return frame_dispatcher_.createListener(h.key(), delegate);
+  }
+
+  // methods from StateInterface
+  virtual bool recover()
+  {
+    return false;
+  };
+
+  virtual State getState()
+  {
+    return state_;
+  };
+
+  virtual void shutdown() {};
+
+  virtual bool translateError(unsigned int internal_error, std::string & str)
+  {
+    if (!internal_error)
+    {
+      str = "OK";
+      return true;
     }
-    bool add(const Frame &k, const std::string &v, bool multi){
-        return add(k, toframe(v), multi);
-    }
-    virtual bool send(const Frame & msg){
-        if(loopback_) frame_dispatcher_.dispatch(msg.key(), msg);
-        try{
-            std::pair <Map::iterator, Map::iterator> r = map_.equal_range(tostring(msg, true));
-            for (Map::iterator it=r.first; it!=r.second; ++it){
-                frame_dispatcher_.dispatch(it->second.key(), it->second);
-            }
-        }
-        catch(const std::out_of_range &e){
-        }
-        return true;
-    }
+    return false;
+  };
 
-    virtual FrameListenerConstSharedPtr createMsgListener(const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(delegate);
-    }
-    virtual FrameListenerConstSharedPtr createMsgListener(const Frame::Header&h , const FrameFunc &delegate){
-        return frame_dispatcher_.createListener(h.key(), delegate);
-    }
+  virtual bool doesLoopBack() const
+  {
+    return loopback_;
+  };
 
-    // methods from StateInterface
-    virtual bool recover(){return false;};
+  virtual void run() {};
 
-    virtual State getState(){return state_;};
+  bool init(const std::string &device, bool loopback)
+  {
+    loopback_ = loopback;
+    state_.driver_state = State::ready;
+    state_.internal_error = 0;
+    state_dispatcher_.dispatch(state_);
+    return true;
+  };
 
-    virtual void shutdown(){};
-
-    virtual bool translateError(unsigned int internal_error, std::string & str){
-        if (!internal_error) {
-            str = "OK";
-            return true;
-        }
-        return false;
-    };
-
-    virtual bool doesLoopBack() const {return loopback_;};
-
-    virtual void run(){};
-
-    bool init(const std::string &device, bool loopback){
-        loopback_ = loopback;
-        state_.driver_state = State::ready;
-        state_.internal_error = 0;
-        state_dispatcher_.dispatch(state_);
-        return true;
-    };
-
-    virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate){
-      return state_dispatcher_.createListener(delegate);
-    };
+  virtual StateListenerConstSharedPtr createStateListener(const StateFunc &delegate)
+  {
+    return state_dispatcher_.createListener(delegate);
+  };
 
 };
 using DummyInterfaceSharedPtr = std::shared_ptr<DummyInterface>;