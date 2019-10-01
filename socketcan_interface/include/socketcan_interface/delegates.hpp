2becfaed1ff66becde04ef6ac804bc848d3051fb
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-09-30 20:12:56 -0500

    Exporting libraries 'the right way.'

--- socketcan_interface/include/socketcan_interface/delegates.hpp
+++ socketcan_interface/include/socketcan_interface/delegates.hpp
@@ -1,3 +1,18 @@
+// Copyright (c) 2016-2019, Fraunhofer, Mathias Lüdtke, AutonomouStuff
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef SOCKETCAN_INTERFACE_DELEGATES_H_
 #define SOCKETCAN_INTERFACE_DELEGATES_H_
 
--- socketcan_interface/test/test_dispatcher.cpp
+++ socketcan_interface/test/test_dispatcher.cpp
@@ -1,8 +1,21 @@
-// Bring in my package's API, which is what I'm testing
-#include <socketcan_interface/dispatcher.hpp>
+// Copyright (c) 2016-2019, Fraunhofer, Mathias Lüdtke, AutonomouStuff
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// Bring in gtest
 #include <gtest/gtest.h>
+
+#include <socketcan_interface/dispatcher.hpp>
 
 class Counter {
 public:
