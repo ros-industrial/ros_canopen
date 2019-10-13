58bfa77e369657a9375a19b0099a505e83a5c089
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-10-14 00:32:53 +0200

    fix roslint errors in socketcan_bridge

--- socketcan_bridge/src/socketcan_to_topic.cpp
+++ socketcan_bridge/src/socketcan_to_topic.cpp
@@ -30,25 +30,30 @@
 #include <can_msgs/Frame.h>
 #include <string>
 
-namespace can {
-template<> can::FrameFilterSharedPtr tofilter(const XmlRpc::XmlRpcValue  &ct) {
+namespace can
+{
+template<> can::FrameFilterSharedPtr tofilter(const XmlRpc::XmlRpcValue  &ct)
+{
   XmlRpc::XmlRpcValue t(ct);
-  try{ // to read as integer
-      uint32_t id = static_cast<int>(t);
-      return tofilter(id);
+  try  // try to read as integer
+  {
+    uint32_t id = static_cast<int>(t);
+    return tofilter(id);
   }
-  catch(...){ // read as string
-      return  tofilter(static_cast<std::string>(t));
+  catch(...)  // else read as string
+  {
+    return  tofilter(static_cast<std::string>(t));
   }
 }
-}
+}  // namespace can
 
 namespace socketcan_bridge
 {
   SocketCANToTopic::SocketCANToTopic(ros::NodeHandle* nh, ros::NodeHandle* nh_param,
       can::DriverInterfaceSharedPtr driver)
     {
-      can_topic_ = nh->advertise<can_msgs::Frame>("received_messages", nh_param->param("received_messages_queue_size", 10));
+      can_topic_ = nh->advertise<can_msgs::Frame>("received_messages",
+                                                  nh_param->param("received_messages_queue_size", 10));
       driver_ = driver;
     };
 
@@ -59,20 +64,25 @@ namespace socketcan_bridge
       state_listener_ = driver_->createStateListenerM(this, &SocketCANToTopic::stateCallback);
     };
 
-  void SocketCANToTopic::setup(const can::FilteredFrameListener::FilterVector &filters){
+  void SocketCANToTopic::setup(const can::FilteredFrameListener::FilterVector &filters)
+  {
     frame_listener_.reset(new can::FilteredFrameListener(driver_,
-                                                         std::bind(&SocketCANToTopic::frameCallback, this, std::placeholders::_1),
+                                                         std::bind(&SocketCANToTopic::frameCallback,
+                                                                   this,
+                                                                   std::placeholders::_1),
                                                          filters));
 
     state_listener_ = driver_->createStateListenerM(this, &SocketCANToTopic::stateCallback);
   }
 
-  void SocketCANToTopic::setup(XmlRpc::XmlRpcValue filters) {
+  void SocketCANToTopic::setup(XmlRpc::XmlRpcValue filters)
+  {
       setup(can::tofilters(filters));
   }
-  void SocketCANToTopic::setup(ros::NodeHandle nh) {
+  void SocketCANToTopic::setup(ros::NodeHandle nh)
+  {
        XmlRpc::XmlRpcValue filters;
-       if(nh.getParam("can_ids", filters)) return setup(filters);
+       if (nh.getParam("can_ids", filters)) return setup(filters);
        return setup();
   }
 
