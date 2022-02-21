09125a233cb358e59b625432a4a3dd836b3516b8
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-02-21 08:11:24 +0100

    update lauunch

--- ros2_canopen_core/launch/bring_up_master_slave.launch.py
+++ ros2_canopen_core/launch/bring_up_master_slave.launch.py
@@ -18,87 +18,34 @@ import lifecycle_msgs.msg
 def generate_launch_description():
     path_to_test = os.path.dirname(__file__)
 
-    parameter_file = launch.substitutions.LaunchConfiguration('parameter_file_path')
-    parameter_file_arg = launch.actions.DeclareLaunchArgument(
-        'parameter_file_path',
-        default_value=''
-    )
     ld = launch.LaunchDescription()
 
-    master_node = launch_ros.actions.LifecycleNode(
-        name="canopen_master",
+    master_node = launch_ros.actions.Node(
+        name="device_manager_node",
         namespace="", 
         package="ros2_canopen_core", 
         output="screen", 
-        executable="canopen_master_node",
+        executable="device_manager_node",
         parameters= [{
-                    "yaml_path": os.path.join(path_to_test, ".." ,  "resources" , "simple.yml"),
-                    "dcf_path": os.path.join(path_to_test, ".." , "resources" , "simple.dcf"),
-                }
+            "dcf_config": os.path.join(path_to_test, ".." ,  "resources" , "proxy.yml"),
+            "dcf_txt": os.path.join(path_to_test, ".." , "resources" , "master.dcf"),
+            "can_interface_name": "vcan0"}
         ],
     )
 
-    master_node_inactive_state_handler = launch.actions.RegisterEventHandler(
-        launch_ros.event_handlers.OnStateTransition(
-            target_lifecycle_node=master_node, goal_state='inactive',
-            entities=[
-                launch.actions.LogInfo(
-                    msg="node 'master_node' reached the 'inactive' state, 'activating'."),
-                launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
-                    lifecycle_node_matcher=launch.events.matches_action(master_node),
-                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
-                )),
-            ],
-        )
-    )
-
-    master_node_configure = launch.actions.EmitEvent(
-        event=launch_ros.events.lifecycle.ChangeState(
-            lifecycle_node_matcher=launch.events.matches_action(master_node),
-            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-        )
-    )
-
-    slave_node_2 = launch_ros.actions.LifecycleNode(
-        name="slave_node_2", 
+    slave_node = launch_ros.actions.Node(
+        name="slave_node", 
         namespace="", 
         package="ros2_canopen_core", 
         output="screen", 
-        executable="test_slave",
-        parameters=[
-            {
-                "eds": os.path.join(path_to_test, ".." , "resources" , "simple.eds"),
-                "slave_id": 2
-            }
+        executable="slave_node",
+        parameters=[{
+                "eds": os.path.join(path_to_test, ".." , "resources" , "slave.eds"),
+                "slave_id": 2}
             ],
     )
 
-    slave_2_inactive_state_handler = launch.actions.RegisterEventHandler(
-        launch_ros.event_handlers.OnStateTransition(
-            target_lifecycle_node=slave_node_2, goal_state='inactive',
-            entities=[
-                launch.actions.LogInfo(
-                    msg="node 'slave_node_2' reached the 'inactive' state, 'activating'."),
-                launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
-                    lifecycle_node_matcher=launch.events.matches_action(slave_node_2),
-                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
-                )),
-            ],
-        )
-    )
-
-    slave_2_configure = launch.actions.EmitEvent(
-        event=launch_ros.events.lifecycle.ChangeState(
-            lifecycle_node_matcher=launch.events.matches_action(slave_node_2),
-            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-        )
-    )
-    ld.add_action(slave_2_inactive_state_handler)
-    ld.add_action(slave_node_2)
-    ld.add_action(slave_2_configure)
-    ld.add_action(parameter_file_arg)
-    ld.add_action(master_node_inactive_state_handler)
+    ld.add_action(slave_node)
     ld.add_action(master_node)
-    ld.add_action(master_node_configure)
 
     return ld
