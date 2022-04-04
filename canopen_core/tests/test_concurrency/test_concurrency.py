74aa6040ef720b5288d40bb8c31299c6eead04c7
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2022-04-04 20:35:07 +0200

    rename packages to fit ROS2 conventions better

---
+++ canopen_core/tests/test_concurrency/test_concurrency.py
@@ -0,0 +1,184 @@
+from asyncio import Future
+from concurrent.futures import thread
+import os
+import sys
+from threading import Thread, Lock
+import time
+import unittest
+import uuid
+from rclpy.executors import MultiThreadedExecutor
+import launch
+import launch_ros
+import launch_ros.actions
+import launch_testing.actions
+import launch_testing_ros
+
+import lifecycle_msgs.msg
+import canopen_interfaces.srv
+
+import pytest
+
+import rclpy
+
+import std_msgs.msg
+import std_srvs.srv
+
+
+@pytest.mark.rostest
+def generate_test_description():
+    path_to_test = os.path.dirname(__file__)
+    print(os.path.join(path_to_test, "master.dcf"))
+
+    ld = launch.LaunchDescription()
+
+    master_node = launch_ros.actions.Node(
+        name="device_manager_node",
+        namespace="",
+        package="canopen_core",
+        output="screen",
+        executable="device_manager_node",
+        parameters=[{
+            "bus_config": os.path.join(path_to_test, "concurrency.yml"),
+            "master_config": os.path.join(path_to_test, "concurrency.dcf"),
+            "can_interface_name": "vcan0"}
+        ],
+    )
+
+    for i in range(2, 7):
+        print(os.path.join(path_to_test, "simle.eds"))
+        slave_node = launch_ros.actions.LifecycleNode(
+            name="slave_node_{}".format(i),
+            namespace="",
+            package="canopen_core",
+            output="screen",
+            executable="slave_node",
+            parameters=[{
+                    "eds": os.path.join(path_to_test, "simple.eds"),
+                    "slave_id": i}
+            ],
+        )
+
+        slave_inactive_state_handler = launch.actions.RegisterEventHandler(
+            launch_ros.event_handlers.OnStateTransition(
+                target_lifecycle_node=slave_node, goal_state='inactive',
+                entities=[
+                    launch.actions.LogInfo(
+                        msg="node 'slave_node_{}' reached the 'inactive' state, 'activating'.".format(i)),
+                    launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
+                        lifecycle_node_matcher=launch.events.matches_action(
+                            slave_node),
+                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                    )),
+                ],
+            )
+        )
+        slave_configure = launch.actions.EmitEvent(
+            event=launch_ros.events.lifecycle.ChangeState(
+                lifecycle_node_matcher=launch.events.matches_action(
+                    slave_node),
+                transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+            )
+        )
+        ld.add_action(slave_inactive_state_handler)
+        ld.add_action(slave_node)
+        ld.add_action(slave_configure)
+
+    ready_to_test = launch.actions.TimerAction(
+        period=10.0,
+        actions=[
+            launch_testing.actions.ReadyToTest()
+        ],
+    )
+    ld.add_action(master_node)
+    ld.add_action(ready_to_test)
+    return (ld, {})
+
+
+class TestBasicDevice(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.nodes = []
+        for i in range(2, 7):
+            self.nodes.append(rclpy.create_node('tester_{}'.format(i)))
+
+    def tearDown(self):
+        for node in self.nodes:
+            node.destroy_node()
+
+    def test_sdo(self, launch_service, proc_output):
+        success_vector = []
+        duration_vector = []
+        mutex = Lock()
+        running = True
+
+        def sdo_write(node, id):
+            client = node.create_client(canopen_interfaces.srv.COWrite,
+                                        "/motioncontroller_{}/sdo_write".format(id))
+            success = True
+            duration = 0.0
+            for i in range(1, 10):
+                print("Call nodeid {} #{}".format(id, i))
+                req = canopen_interfaces.srv.COWrite.Request()
+                req.data = 1000
+                req.index = 0x1017
+                req.subindex = 0
+                req.type = 16
+                result = client.call(req)
+                if not result.success:
+                    print("Call nodeid {} #{} failed with {}".format(id, i))
+                    success = False
+                    break
+                time.sleep(0.01)
+
+            success_vector[id-2] = success
+
+        def spin_executor(exec):
+            mutex.acquire()
+            while(running):
+                mutex.release()
+                exec.spin_once(1.0)
+                mutex.acquire()
+
+        exec = MultiThreadedExecutor()
+        threads = []
+        for node in self.nodes:
+            exec.add_node(node)
+            success_vector.append(False)
+
+        for i in range(2, 7):
+            print("Add thread")
+            t = Thread(target=sdo_write, args=(self.nodes[i-2], i, ))
+            threads.append(t)
+
+        spinner = Thread(target=spin_executor, args=(exec, ))
+        print("Start Executor")
+        spinner.start()
+
+        time.sleep(2.0)
+
+        for t in threads:
+            print("Start Thread")
+            t.start()
+
+        for t in threads:
+            print("Finished Thread")
+            t.join()
+            
+        mutex.acquire()
+        running = False
+        mutex.release()
+
+        print("Finished Executor")
+        spinner.join()
+
+        for success in success_vector:
+            print(success)
+            self.assertTrue(success)
---
+++ canopen_proxy_driver/include/canopen_proxy_driver/visibility_control.h
@@ -0,0 +1,48 @@
+//    Copyright 2022 Christoph Hellmann Santos
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#ifndef CANOPEN_PROXY_DRIVER__VISIBILITY_CONTROL_H_
+#define CANOPEN_PROXY_DRIVER__VISIBILITY_CONTROL_H_
+
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define CANOPEN_PROXY_DRIVER_EXPORT __attribute__ ((dllexport))
+    #define CANOPEN_PROXY_DRIVER_IMPORT __attribute__ ((dllimport))
+  #else
+    #define CANOPEN_PROXY_DRIVER_EXPORT __declspec(dllexport)
+    #define CANOPEN_PROXY_DRIVER_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef CANOPEN_PROXY_DRIVER_BUILDING_LIBRARY
+    #define CANOPEN_PROXY_DRIVER_PUBLIC CANOPEN_PROXY_DRIVER_EXPORT
+  #else
+    #define CANOPEN_PROXY_DRIVER_PUBLIC CANOPEN_PROXY_DRIVER_IMPORT
+  #endif
+  #define CANOPEN_PROXY_DRIVER_PUBLIC_TYPE CANOPEN_PROXY_DRIVER_PUBLIC
+  #define CANOPEN_PROXY_DRIVER_LOCAL
+#else
+  #define CANOPEN_PROXY_DRIVER_EXPORT __attribute__ ((visibility("default")))
+  #define CANOPEN_PROXY_DRIVER_IMPORT
+  #if __GNUC__ >= 4
+    #define CANOPEN_PROXY_DRIVER_PUBLIC __attribute__ ((visibility("default")))
+    #define CANOPEN_PROXY_DRIVER_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define CANOPEN_PROXY_DRIVER_PUBLIC
+    #define CANOPEN_PROXY_DRIVER_LOCAL
+  #endif
+  #define CANOPEN_PROXY_DRIVER_PUBLIC_TYPE
+#endif
+
+#endif  // CANOPEN_PROXY_DRIVER__VISIBILITY_CONTROL_H_
