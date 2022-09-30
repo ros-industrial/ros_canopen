2b7449a1f050a761dcf16a9b10539ed94008ce23
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2022-09-30 09:42:14 +0200

    Add CanopenDriverInterface Documentation

--- canopen_core/include/canopen_core/driver_node.hpp
+++ canopen_core/include/canopen_core/driver_node.hpp
@@ -10,12 +10,65 @@ namespace ros2_canopen
     class CanopenDriverInterface
     {
     public:
+        /**
+         * @brief Initialise the driver
+         * 
+         * This function will initialise the drivers functionalities.
+         * It will be called by the device_container when the node has
+         * been added to the executor.
+         */
         virtual void init() = 0;
+
+        /**
+         * @brief Set the master object
+         * 
+         * This function will set the Canopen Master Objects that are
+         * necessary for the driver to be instantiated. It will be called
+         * by the device container when the init_driver service is invoked.
+         * 
+         * @param exec 
+         * @param master 
+         */
         virtual void set_master(std::shared_ptr<lely::ev::Executor> exec,
                                 std::shared_ptr<lely::canopen::AsyncMaster> master) = 0;
+
+        /**
+         * @brief Get the node base interface object
+         * 
+         * This function shall return an rclcpp::node_interfaces::NodeBaseInterface::SharedPtr.
+         * The pointer will be used to add the driver node to the executor.
+         * 
+         * @return rclcpp::node_interfaces::NodeBaseInterface::SharedPtr 
+         */
         virtual rclcpp::node_interfaces::NodeBaseInterface::SharedPtr get_node_base_interface() = 0;
+
+        /**
+         * @brief Shutdown the driver
+         * 
+         * This function will shutdown the driver and will especially
+         * join all threads to enable a clean shutdown.
+         * 
+         */
         virtual void shutdown() = 0;
+
+        /**
+         * @brief Check whether this is a LifecycleNode
+         * 
+         * This function provides runtime information on whether the driver
+         * is a lifecycle driver or not.
+         * 
+         * @return true 
+         * @return false 
+         */
         virtual bool is_lifecycle() = 0;
+
+        /**
+         * @brief Get the node canopen driver interface object
+         * 
+         * This function gives access to the underlying NodeCanopenDriverInterface.
+         * 
+         * @return std::shared_ptr<node_interfaces::NodeCanopenDriverInterface> 
+         */
         virtual std::shared_ptr<node_interfaces::NodeCanopenDriverInterface> get_node_canopen_driver_interface() = 0;
 
     };
