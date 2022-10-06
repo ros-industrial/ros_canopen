96ab6fa9ecaaad3592762b1bce42649536107473
Lovro <lovro.ivanov@gmail.com>
2022-10-06 16:18:10 +0200

    Add base function ret values first.

--- canopen_ros2_controllers/src/cia402_device_controller.cpp
+++ canopen_ros2_controllers/src/cia402_device_controller.cpp
@@ -28,27 +28,15 @@ static constexpr double kCommandValue = 1.0;
 namespace
 {  // utility
 
-/*
+
 using ControllerCommandMsg = canopen_ros2_controllers::CanopenProxyController::ControllerCommandMsg;
 
 // called from RT control loop
 void reset_controller_command_msg(
   std::shared_ptr<ControllerCommandMsg> & msg, const std::string & joint_name)
 {
-  msg->index = 0u;
-  msg->subindex = 0u;
-  msg->type = 0u;
-  msg->data = 0u;
 }
-bool propagate_controller_command_msg(
-  std::shared_ptr<ControllerCommandMsg> & msg)
-{
-  // TODO (livanov93): add better logic to decide if a message
-  //  should be propagated to the bus
-  // check if it is 8 = uint8_t or 16 = uint16_t or 32 = uint32_t
-  return msg->type == 8 || msg->type == 16 || msg->type == 32;
-}
-*/
+
 }  // namespace
 
 namespace canopen_ros2_controllers
@@ -57,35 +45,44 @@ Cia402DeviceController::Cia402DeviceController() {
 }
 
 controller_interface::CallbackReturn Cia402DeviceController::on_init() {
-    return CanopenProxyController::on_init();
+    auto ret_val =  CanopenProxyController::on_init();
+    return ret_val;
 }
 
 controller_interface::InterfaceConfiguration Cia402DeviceController::command_interface_configuration() const {
-    return CanopenProxyController::command_interface_configuration();
+    auto command_interfaces_config = CanopenProxyController::command_interface_configuration();
+    command_interfaces_config.names.emplace_back("/");
+    return command_interfaces_config;
 }
 
 controller_interface::InterfaceConfiguration Cia402DeviceController::state_interface_configuration() const {
-    return CanopenProxyController::state_interface_configuration();
+    auto state_interfaces_config = CanopenProxyController::state_interface_configuration();
+    state_interfaces_config.names.emplace_back("/");
+    return state_interfaces_config;
 }
 
 controller_interface::CallbackReturn
 Cia402DeviceController::on_configure(const rclcpp_lifecycle::State &previous_state) {
-    return CanopenProxyController::on_configure(previous_state);
+    auto ret_val = CanopenProxyController::on_configure(previous_state);
+    return ret_val;
 }
 
 controller_interface::CallbackReturn
 Cia402DeviceController::on_activate(const rclcpp_lifecycle::State &previous_state) {
-    return CanopenProxyController::on_activate(previous_state);
+    auto ret_val = CanopenProxyController::on_activate(previous_state);
+    return ret_val;
 }
 
 controller_interface::CallbackReturn
 Cia402DeviceController::on_deactivate(const rclcpp_lifecycle::State &previous_state) {
-    return CanopenProxyController::on_deactivate(previous_state);
+    auto ret_val = CanopenProxyController::on_deactivate(previous_state);
+    return ret_val;
 }
 
 controller_interface::return_type
 Cia402DeviceController::update(const rclcpp::Time &time, const rclcpp::Duration &period) {
-    return CanopenProxyController::update(time, period);
+    auto ret_val = CanopenProxyController::update(time, period);
+    return ret_val;
 }
 
 }  // namespace canopen_ros2_controllers
